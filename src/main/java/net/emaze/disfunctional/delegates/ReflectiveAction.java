4c5d8398a126fb196141493fc255c6f3b81b969a
Roberto Ferranti <rf@emaze.net>
2010-08-31 18:18:04 +0200

    ref: MethodFetcher

--- src/main/java/net/emaze/disfunctional/delegates/ReflectiveAction.java
+++ src/main/java/net/emaze/disfunctional/delegates/ReflectiveAction.java
@@ -2,11 +2,9 @@ package net.emaze.disfunctional.delegates;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
 import net.emaze.disfunctional.Maybe;
 import net.emaze.disfunctional.Memoized;
-import net.emaze.disfunctional.iterations.Iterations;
+import net.emaze.disfunctional.reflection.MethodFetcher;
 
 /**
  *
@@ -19,50 +17,24 @@ public class ReflectiveAction<T> implements Action<T> {
     private Maybe<Class<T>> clazz = Maybe.nothing();
     private Memoized<Method> methodCache = new Memoized<Method>();
 
-    private static <T> Method fetchMethod(Object callee, String methodName, Class<T> paramClass, T message)
-            throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        return callee.getClass().getMethod(methodName, paramClass);
-    }
-
-    private static <T> Method fetchMethod(Object callee, final String methodName, T message)
-            throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        final List<Method> methods = Arrays.asList(callee.getClass().getMethods());
-        final List<Method> matchingMethods = Iterations.some(methods, new Predicate<Method>() {
-
-            public boolean test(Method method) {
-                return method.getName().equals(methodName);
-            }
-        });
-
-        if (matchingMethods.isEmpty()) {
-            throw new IllegalStateException("trying to call by reflection an inexistent method");
-        }
-        if (matchingMethods.size() > 1) {
-            throw new IllegalStateException("trying to call by reflection on an ambigouous method (messageClass must be specified)");
-        }
-        return matchingMethods.get(0);
-    }
-
     @Override
     public void perform(T message) {
         try {
             if (!methodCache.isMemoized()) {
                 final Method m = clazz.hasValue()
-                        ? fetchMethod(callee, methodName, clazz.value(), message)
-                        : fetchMethod(callee, methodName, message);
+                        ? new MethodFetcher().fetch(callee.getClass(), methodName, clazz.value())
+                        : new MethodFetcher().fetchAmbiguous(callee.getClass(), methodName);
                 methodCache.memoize(m);
             }
             methodCache.value().invoke(callee, message);
         } catch (IllegalAccessException ex) {
-            throw new IllegalStateException("", ex);
+            throw new IllegalStateException(ex);
         } catch (IllegalArgumentException ex) {
-            throw new IllegalStateException("", ex);
+            throw new IllegalStateException(ex);
         } catch (InvocationTargetException ex) {
-            throw new IllegalStateException("", ex);
-        } catch (NoSuchMethodException ex) {
-            throw new IllegalStateException("", ex);
+            throw new IllegalStateException(ex);
         } catch (SecurityException ex) {
-            throw new IllegalStateException("", ex);
+            throw new IllegalStateException(ex);
         }
     }
 
