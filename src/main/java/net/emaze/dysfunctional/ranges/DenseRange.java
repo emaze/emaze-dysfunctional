1bd607c947a77bebdb5ed3febb9f1990f3375d64
Roberto Ferranti <rf@emaze.net>
2010-09-13 10:34:20 +0200

    enh: equals and hashcode for {Dense,Sparse}Range

--- src/main/java/net/emaze/dysfunctional/ranges/DenseRange.java
+++ src/main/java/net/emaze/dysfunctional/ranges/DenseRange.java
@@ -2,19 +2,21 @@ package net.emaze.dysfunctional.ranges;
 
 import net.emaze.dysfunctional.iterations.RangeIterator;
 import java.util.Iterator;
+import net.emaze.dysfunctional.concepts.EqualsBuilder;
+import net.emaze.dysfunctional.concepts.HashCodeBuilder;
 import net.emaze.dysfunctional.contracts.dbc;
 
 /**
  *
  * @author rferranti
  */
-public class DenseRange <T extends Comparable<T>> implements Range<T> {
+public class DenseRange<T extends Comparable<T>> implements Range<T> {
 
     private final T lower;
     private final T upper;
     private final RangePolicy<T> policy;
 
-    public DenseRange(RangePolicy<T> policy, T lower, T upper){
+    public DenseRange(RangePolicy<T> policy, T lower, T upper) {
         dbc.precondition(policy != null, "trying to create a DenseRange with a null RangePolicy");
         dbc.precondition(lower != null, "trying to create a DenseRange with null lower bound");
         dbc.precondition(upper != null, "trying to create a DenseRange with null upper bound");
@@ -23,10 +25,9 @@ public class DenseRange <T extends Comparable<T>> implements Range<T> {
         this.upper = upper;
     }
 
-
     @Override
     public boolean contains(T element) {
-        return policy.contains(this,element);
+        return policy.contains(this, element);
     }
 
     @Override
@@ -49,4 +50,25 @@ public class DenseRange <T extends Comparable<T>> implements Range<T> {
         return new RangeIterator(policy, lower, upper);
     }
 
+    @Override
+    public boolean equals(Object rhs) {
+        if (rhs instanceof DenseRange == false) {
+            return false;
+        }
+        final DenseRange<T> other = (DenseRange<T>) rhs;
+        return new EqualsBuilder().
+                append(this.policy, other.policy).
+                append(this.lower, other.lower).
+                append(this.upper, other.upper).
+                isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().
+                append(policy).
+                append(lower).
+                append(upper).
+                toHashCode();
+    }
 }
--- src/main/java/net/emaze/dysfunctional/ranges/SparseRange.java
+++ src/main/java/net/emaze/dysfunctional/ranges/SparseRange.java
@@ -3,6 +3,8 @@ package net.emaze.dysfunctional.ranges;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import net.emaze.dysfunctional.concepts.EqualsBuilder;
+import net.emaze.dysfunctional.concepts.HashCodeBuilder;
 import net.emaze.dysfunctional.contracts.dbc;
 import net.emaze.dysfunctional.delegates.Delegate;
 import net.emaze.dysfunctional.delegates.Predicate;
@@ -17,9 +19,9 @@ public class SparseRange<T extends Comparable<T>> implements Range<T> {
 
     private final List<Range<T>> ranges = new ArrayList<Range<T>>();
     private final RangePolicy<T> policy;
-    
+
     public SparseRange(RangePolicy<T> policy, Range<T>... ranges) {
-        dbc.precondition(ranges.length != 0,"SparseRange<T> must be constructed with at least one argument");
+        dbc.precondition(ranges.length != 0, "SparseRange<T> must be constructed with at least one argument");
         this.policy = policy;
         this.ranges.addAll(policy.normalize(ranges));
     }
@@ -44,6 +46,7 @@ public class SparseRange<T extends Comparable<T>> implements Range<T> {
     public T upper() {
         return ranges.get(ranges.size() - 1).upper();
     }
+
     @Override
     public int compareTo(Range<T> other) {
         return policy.compare(this, other);
@@ -51,7 +54,8 @@ public class SparseRange<T extends Comparable<T>> implements Range<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return new ChainIterator<T>(Iterations.map(ranges , new Delegate< Iterator<T>,Range<T>>(){
+        return new ChainIterator<T>(Iterations.map(ranges, new Delegate<Iterator<T>, Range<T>>() {
+
             @Override
             public Iterator<T> perform(Range<T> iterable) {
                 return iterable.iterator();
@@ -59,4 +63,21 @@ public class SparseRange<T extends Comparable<T>> implements Range<T> {
         }));
     }
 
+    @Override
+    public boolean equals(Object rhs) {
+        if (rhs instanceof SparseRange == false) {
+            return false;
+        }
+        final SparseRange<T> other = (SparseRange<T>) rhs;
+        return new EqualsBuilder().append(this.policy, other.policy).
+                append(this.ranges, other.ranges).
+                isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(policy).
+                append(this.ranges).
+                toHashCode();
+    }
 }
