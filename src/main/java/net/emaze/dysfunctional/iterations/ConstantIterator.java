8f4f392f5625af9fcfc45ddba4ca1e8a8636edf5
Roberto Ferranti <rf@emaze.net>
2010-09-21 15:45:16 +0200

    enh: constant and interleaving iterator

---
+++ src/main/java/net/emaze/dysfunctional/iterations/ConstantIterator.java
@@ -0,0 +1,36 @@
+package net.emaze.dysfunctional.iterations;
+
+import java.util.Iterator;
+
+/**
+ * An infinite Iterator always yield the same value
+ * @author rferranti
+ */
+public class ConstantIterator<T> implements Iterator<T> {
+
+    private final T value;
+
+    public ConstantIterator(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public T next() {
+        return value;
+    }
+
+    /**
+     * Ignore semantic (vs fail).
+     * Removing from a infinite constant iterator just `removes` one element
+     * and leaves everything exactly as it was.
+     */
+    @Override
+    public void remove() {
+        
+    }
+}
