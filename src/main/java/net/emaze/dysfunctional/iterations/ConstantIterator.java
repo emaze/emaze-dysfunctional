ee36867b22e021503c0fad6dfe3b5efdce5abaca
Roberto Ferranti <rf@emaze.net>
2010-09-22 14:19:31 +0200

    doc: typo

--- src/main/java/net/emaze/dysfunctional/iterations/ConstantIterator.java
+++ src/main/java/net/emaze/dysfunctional/iterations/ConstantIterator.java
@@ -3,7 +3,7 @@ package net.emaze.dysfunctional.iterations;
 import java.util.Iterator;
 
 /**
- * An infinite Iterator always yield the same value
+ * An infinite Iterator always yields the same value
  * @author rferranti
  */
 public class ConstantIterator<T> implements Iterator<T> {
