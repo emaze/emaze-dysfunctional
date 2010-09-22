20de34ef92d29a1497084abc789a48190115cffc
Roberto Ferranti <rf@emaze.net>
2010-09-22 14:20:12 +0200

    enh: comparablecomparator implementation

---
+++ src/main/java/net/emaze/dysfunctional/concepts/ComparableComparator.java
@@ -0,0 +1,16 @@
+package net.emaze.dysfunctional.concepts;
+
+import java.util.Comparator;
+
+/**
+ * Redispatching comparator for Comparables
+ * @author rferranti
+ */
+public class ComparableComparator<T extends Comparable> implements Comparator<T>{
+
+    @Override
+    public int compare(T lhs, T rhs) {
+        return lhs.compareTo(rhs);
+    }
+
+}
