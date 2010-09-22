ad7bdf4f3532c93e1444e220007c822811b9f921
Roberto Ferranti <rf@emaze.net>
2010-09-22 14:18:13 +0200

    enh: ArrayIterator and ArrayIteratorAdapter implementations (+testunit)

---
+++ src/main/java/net/emaze/dysfunctional/iterations/ArrayIterator.java
@@ -0,0 +1,45 @@
+package net.emaze.dysfunctional.iterations;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import net.emaze.dysfunctional.contracts.dbc;
+
+/**
+ *
+ * @author rferranti
+ */
+public class ArrayIterator<T> implements Iterator<T> {
+    
+    private final T[] values;
+    private int currentIndex = 0;
+    
+    public ArrayIterator(T[] values) {
+        dbc.precondition(values != null, "trying to create an ArrayIterator<T> from a null array");
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !isOutOfBounds();
+    }
+
+    private boolean isOutOfBounds(){
+        return currentIndex == values.length;
+    }
+
+    @Override
+    public T next() {
+        if(isOutOfBounds()){
+            throw new NoSuchElementException();
+        }
+        final T element = values[currentIndex];
+        ++currentIndex;
+        return element;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/main/java/net/emaze/dysfunctional/iterations/ArrayToIterableAdapter.java
@@ -0,0 +1,23 @@
+package net.emaze.dysfunctional.iterations;
+
+import java.util.Iterator;
+import net.emaze.dysfunctional.contracts.dbc;
+
+/**
+ *
+ * @author rferranti
+ */
+public class ArrayToIterableAdapter<T> implements Iterable<T> {
+
+    private final T[] values;
+
+    public ArrayToIterableAdapter(T[] values) {
+        dbc.precondition(values != null, "trying to create an ArrayToIterableAdapter<T> from a null array");
+        this.values = values;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayIterator(values);
+    }
+}
