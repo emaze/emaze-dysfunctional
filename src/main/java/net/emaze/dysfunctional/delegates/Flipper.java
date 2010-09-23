6ed653a2f318226e6a3de4ca5ae5eadb76558ac9
Roberto Ferranti <rf@emaze.net>
2010-09-23 16:45:23 +0200

    enh: flipper implementation

---
+++ src/main/java/net/emaze/dysfunctional/delegates/Flipper.java
@@ -0,0 +1,21 @@
+package net.emaze.dysfunctional.delegates;
+
+/**
+ * BinaryDelegate<R,U,T> to BinaryDelegate<R,T,U> adapter
+ * (just swaps formal params)
+ * == No dolphins were harmed during the making of this functor ==
+ * @author rferranti
+ */
+public class Flipper<R, T, U> implements BinaryDelegate<R, T, U> {
+
+    private final BinaryDelegate<R, U, T> delegate;
+
+    public Flipper(BinaryDelegate<R, U, T> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public R perform(T former, U latter) {
+        return delegate.perform(latter, former);
+    }
+}
