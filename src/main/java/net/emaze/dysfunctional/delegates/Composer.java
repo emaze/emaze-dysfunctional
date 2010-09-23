3facba28c979f37f64fe0e957363d40dde61d0d7
Roberto Ferranti <rf@emaze.net>
2010-09-23 15:55:10 +0200

    enh: composing functor implementation

---
+++ src/main/java/net/emaze/dysfunctional/delegates/Composer.java
@@ -0,0 +1,22 @@
+package net.emaze.dysfunctional.delegates;
+
+/**
+ * given f, g yields f ° g (f of g, f following g)
+ * @author rferranti
+ */
+public class Composer<R,T,U> implements Delegate<R,T>{
+
+    private final Delegate<U,T> g;
+    private final Delegate<R,U> f;
+
+    public Composer(Delegate<R, U> f, Delegate<U, T> g) {
+        this.f = f;
+        this.g = g;
+    }
+
+    @Override
+    public R perform(T t) {
+        return f.perform(g.perform(t));
+    }
+   
+}
