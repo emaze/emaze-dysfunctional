58955c9f964276cd94f0d83cc86728268560f00a
Roberto Ferranti <rf@emaze.net>
2010-09-29 12:18:23 +0200

    enh: missing implementation of MemoryIterator

---
+++ src/main/java/net/emaze/dysfunctional/filtering/MemoryIterator.java
@@ -0,0 +1,60 @@
+package net.emaze.dysfunctional.filtering;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import net.emaze.dysfunctional.contracts.dbc;
+import net.emaze.dysfunctional.options.Memoized;
+
+/**
+ * TODO: refactor: rename
+ * @author rferranti
+ */
+public class MemoryIterator<T> implements Iterator<T> {
+
+    private final Iterator<T> iterator;
+    private final Memoized<Queue<T>> memory = new Memoized<Queue<T>>();
+    private final int memorySize;
+
+    public MemoryIterator(Iterator<T> iterator, int memorySize) {
+        this.iterator = iterator;
+        this.memorySize = memorySize;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(!memory.isMemoized()){
+            memory.memoize(fetch(iterator,memorySize));
+        }
+        return !memory.value().isEmpty();
+    }
+
+
+    private static <T> Queue<T> fetch(Iterator<T> iterator, int size){
+        final Queue<T> mem = new LinkedList<T>();
+        while(iterator.hasNext()){
+            final T element = iterator.next();
+            mem.add(element);
+            if(mem.size() > size){
+                mem.remove();
+            }
+        }
+        dbc.stateprecondition(mem.size() == size, "iterator too short");
+        return mem;
+    }
+
+
+
+    @Override
+    public T next() {
+        if(!memory.isMemoized()){
+            memory.memoize(fetch(iterator, memorySize));
+        }
+        return memory.value().remove();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
