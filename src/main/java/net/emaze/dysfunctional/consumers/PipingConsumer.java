b95fc37853ed6aa2f45d5964504ec1035dda361d
Roberto Ferranti <rf@emaze.net>
2010-10-21 11:12:29 +0200

    enh: a piping consumer

---
+++ src/main/java/net/emaze/dysfunctional/consumers/PipingConsumer.java
@@ -0,0 +1,29 @@
+package net.emaze.dysfunctional.consumers;
+
+import java.util.Iterator;
+import net.emaze.dysfunctional.contracts.dbc;
+import net.emaze.dysfunctional.iterations.OutputIterator;
+
+/**
+ *
+ * @param <E>
+ * @author rferranti
+ */
+public class PipingConsumer<E> implements Consumer<OutputIterator<E>, Iterator<E>> {
+
+    private final OutputIterator<E> output;
+
+    public PipingConsumer(OutputIterator<E> output) {
+        this.output = output;
+    }
+
+    @Override
+    public OutputIterator<E> consume(Iterator<E> consumable) {
+        dbc.precondition(consumable != null, "consuming a null iterator");
+        while(consumable.hasNext()){
+            dbc.stateprecondition(output.hasNext(), "outputIterator is smaller than the inputIterator ");
+            output.next(consumable.next());
+        }
+        return output;
+    }
+}
