4def0f67f58aefaae63eb5639eb213f8346cbb04
Roberto Ferranti <rf@emaze.net>
2010-12-09 15:16:10 +0100

    enh: unit tests

---
+++ src/test/java/net/emaze/dysfunctional/order/StrictOrderingDoubleComparatorTest.java
@@ -0,0 +1,62 @@
+package net.emaze.dysfunctional.order;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author rferranti
+ */
+public class StrictOrderingDoubleComparatorTest {
+
+    private final StrictOrderingDoubleComparator comparator = new StrictOrderingDoubleComparator();
+
+    @Test
+    public void positiveZeroIsGreaterThanNegativeZero() {
+        Assert.assertEquals(Comparing.LHS_IS_LESSER,comparator.compare(-0.0, +0.0));
+    }
+    
+    @Test
+    public void oneIsGreaterThanPositiveZero() {
+        Assert.assertEquals(Comparing.LHS_IS_GREATER,comparator.compare(1., +0.0));
+    }
+    @Test
+    public void oneIsGreaterThanNegativeZero() {
+        Assert.assertEquals(Comparing.LHS_IS_GREATER,comparator.compare(1., -0.0));
+    }
+    
+    @Test
+    public void negativeOneIsLesserThanPositiveZero() {
+        Assert.assertEquals(Comparing.LHS_IS_LESSER,comparator.compare(-1., +0.0));
+    }
+    
+    @Test
+    public void positiveOneIsLesserThanPositiveZero() {
+        Assert.assertEquals(Comparing.LHS_IS_GREATER,comparator.compare(+1., -0.0));
+    }
+    
+    @Test
+    public void notANumberIsEqualsToHimself() {
+        Assert.assertFalse(Float.NaN == Float.NaN);
+        Assert.assertEquals(Comparing.SAME_ORDER,comparator.compare(Double.NaN, Double.NaN));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void comparingWithNullLhsYieldsException(){
+        comparator.compare(null, 0.);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void comparingWithNullRhsYieldsException(){
+        comparator.compare(0., null);
+    }
+    
+    @Test
+    public void twoComparatorsHaveSameHashCode(){
+        Assert.assertEquals(new StrictOrderingDoubleComparator().hashCode(), new StrictOrderingDoubleComparator().hashCode());
+    }
+    @Test
+    public void twoComparatorsAreEquals(){
+        Assert.assertEquals(new StrictOrderingDoubleComparator(), new StrictOrderingDoubleComparator());
+    }
+}
--- src/test/java/net/emaze/dysfunctional/order/StrictOrderingFloatComparatorTest.java
+++ src/test/java/net/emaze/dysfunctional/order/StrictOrderingFloatComparatorTest.java
@@ -11,38 +11,52 @@ public class StrictOrderingFloatComparatorTest {
 
     private final StrictOrderingFloatComparator comparator = new StrictOrderingFloatComparator();
 
-    private static final int FORMER_IS_GREATER = 1;
-    private static final int LATTER_IS_GREATER = -1;
-    private static final int ELEMENTS_ARE_EQUAL = 0;
-
-
     @Test
     public void positiveZeroIsGreaterThanNegativeZero() {
-        Assert.assertEquals(LATTER_IS_GREATER,comparator.compare(-0.0f, +0.0f));
+        Assert.assertEquals(Comparing.LHS_IS_LESSER,comparator.compare(-0.0f, +0.0f));
     }
     
     @Test
     public void oneIsGreaterThanPositiveZero() {
-        Assert.assertEquals(FORMER_IS_GREATER,comparator.compare(1f, +0.0f));
+        Assert.assertEquals(Comparing.LHS_IS_GREATER,comparator.compare(1f, +0.0f));
     }
     @Test
     public void oneIsGreaterThanNegativeZero() {
-        Assert.assertEquals(FORMER_IS_GREATER,comparator.compare(1f, -0.0f));
+        Assert.assertEquals(Comparing.LHS_IS_GREATER,comparator.compare(1f, -0.0f));
     }
     
     @Test
     public void negativeOneIsLesserThanPositiveZero() {
-        Assert.assertEquals(LATTER_IS_GREATER,comparator.compare(-1f, +0.0f));
+        Assert.assertEquals(Comparing.LHS_IS_LESSER,comparator.compare(-1f, +0.0f));
     }
     
     @Test
     public void positiveOneIsLesserThanPositiveZero() {
-        Assert.assertEquals(FORMER_IS_GREATER,comparator.compare(+1f, -0.0f));
+        Assert.assertEquals(Comparing.LHS_IS_GREATER,comparator.compare(+1f, -0.0f));
     }
     
     @Test
     public void notANumberIsEqualsToHimself() {
         Assert.assertFalse(Float.NaN == Float.NaN);
-        Assert.assertEquals(ELEMENTS_ARE_EQUAL,comparator.compare(Float.NaN, Float.NaN));
+        Assert.assertEquals(Comparing.SAME_ORDER,comparator.compare(Float.NaN, Float.NaN));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void comparingWithNullLhsYieldsException(){
+        comparator.compare(null, 0f);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void comparingWithNullRhsYieldsException(){
+        comparator.compare(0f, null);
+    }
+    
+    @Test
+    public void twoComparatorsHaveSameHashCode(){
+        Assert.assertEquals(new StrictOrderingFloatComparator().hashCode(), new StrictOrderingFloatComparator().hashCode());
+    }
+    @Test
+    public void twoComparatorsAreEquals(){
+        Assert.assertEquals(new StrictOrderingFloatComparator(), new StrictOrderingFloatComparator());
     }
 }
