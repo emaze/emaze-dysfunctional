006865f5c3da64951bab9fc8441df8d37d19fff5
Roberto Ferranti <rf@emaze.net>
2010-12-10 16:57:15 +0100

    enh: coverage on Strings

--- src/test/java/net/emaze/dysfunctional/strings/StringsTest.java
+++ src/test/java/net/emaze/dysfunctional/strings/StringsTest.java
@@ -3,6 +3,7 @@ package net.emaze.dysfunctional.strings;
 import java.util.Arrays;
 import java.util.Iterator;
 import junit.framework.Assert;
+import net.emaze.dysfunctional.iterations.ConstantIterator;
 import org.junit.Test;
 
 /**
@@ -12,23 +13,50 @@ import org.junit.Test;
 public class StringsTest {
 
     @Test
-    public void testThatCanInterposeIntegers() {
+    public void canInterposeIntegers() {
         Iterator<Integer> values = Arrays.asList(1, 2, 3, 4, 5).iterator();
         String output = Strings.interpose(values, 0);
         Assert.assertEquals("102030405", output);
     }
+    @Test
+    public void canInterposeWithMultipleSeparators() {
+        Iterator<Integer> values = Arrays.asList(1, 2, 3, 4, 5).iterator();
+        Iterator<String> separators = Arrays.asList("-", "+", "-", "+").iterator();
+        String output = Strings.interpose(values, separators);
+        Assert.assertEquals("1-2+3-4+5", output);
+    }
 
     @Test
-    public void testThatCanJoinIntegers() {
+    public void canJoinIntegers() {
         Iterator<Integer> values = Arrays.asList(1, 2, 3, 4).iterator();
         String output = Strings.join(values);
         Assert.assertEquals("1234", output);
     }
 
     @Test
-    public void testThatCanJoinEmptyIterator() {
+    public void canJoinEmptyIterator() {
         Iterator<Object> values = Arrays.asList().iterator();
         String output = Strings.join(values);
         Assert.assertEquals("", output);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void interposingNullValuesYieldsException() {
+        Strings.interpose(null, 0);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void interposingNullValuesYieldsExceptionOverload() {
+        Strings.interpose(null, new ConstantIterator<Integer>(0));
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void interposingNullSeparatorYieldsException() {
+        Strings.interpose(Arrays.<String>asList().iterator(), null);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void interposingNullSeparatorsYieldsException() {
+        Strings.interpose(Arrays.<String>asList().iterator(), (Iterator<String>)null);
     }
 }
