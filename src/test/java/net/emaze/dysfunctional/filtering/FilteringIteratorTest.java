654eb10f50346e3cc83f6800b923569c2e504486
Roberto Ferranti <rf@emaze.net>
2010-12-10 14:12:20 +0100

    enh: coverage on filteringiterator

---
+++ src/test/java/net/emaze/dysfunctional/filtering/FilteringIteratorTest.java
@@ -0,0 +1,62 @@
+package net.emaze.dysfunctional.filtering;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import net.emaze.dysfunctional.delegates.Always;
+import net.emaze.dysfunctional.delegates.Never;
+import net.emaze.dysfunctional.iterations.ConstantIterator;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author rferranti
+ */
+public class FilteringIteratorTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingFilteringIteratorWithNullIteratoryYieldsException() {
+        new FilteringIterator<Object>(null, new Always<Object>());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingFilteringIteratorWithNullPredicateYieldsException() {
+        new FilteringIterator<Object>(new ConstantIterator<Object>("a"), null);
+    }
+
+    @Test
+    public void callingHasNextDoesNotConsumeValues() {
+        List<Integer> bucket = Arrays.asList(1);
+        Iterator<Integer> iter = new FilteringIterator<Integer>(bucket.iterator(), new Always<Integer>());
+        iter.hasNext();
+        iter.hasNext();
+        Assert.assertEquals(Integer.valueOf(1), iter.next());
+    }
+
+    @Test
+    public void emptyIteratorHasNoNext() {
+        List<Integer> bucket = Collections.emptyList();
+        Iterator<Integer> iter = new FilteringIterator<Integer>(bucket.iterator(), new Always<Integer>());
+        Assert.assertFalse(iter.hasNext());
+    }
+
+    @Test
+    public void whenPredicateEvaluatesToFalseElementsAreFilteredOut() {
+        List<Integer> bucket = Collections.singletonList(1);
+        Iterator<Integer> iter = new FilteringIterator<Integer>(bucket.iterator(), new Never<Integer>());
+        Assert.assertFalse(iter.hasNext());
+    }
+
+    @Test
+    public void canCallNextMultipleTimesWithoutCallingHasNext() {
+        List<Integer> bucket = Arrays.asList(1, 2);
+        Iterator<Integer> iter = new FilteringIterator<Integer>(bucket.iterator(), new Always<Integer>());
+        List<Integer> got = new ArrayList<Integer>();
+        got.add(iter.next());
+        got.add(iter.next());
+        Assert.assertEquals(bucket, got);
+    }
+}
