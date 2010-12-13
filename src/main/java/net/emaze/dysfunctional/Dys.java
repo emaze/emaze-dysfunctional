cdcf57d1329879565f0b71c482cc78bbb6082659
Roberto Ferranti <rf@emaze.net>
2010-12-13 15:22:19 +0100

    enh: library facade

---
+++ src/main/java/net/emaze/dysfunctional/Dys.java
@@ -0,0 +1,15 @@
+package net.emaze.dysfunctional;
+
+/**
+ *
+ * @author rferranti
+ */
+public abstract class Dys {
+    public abstract static class Zips extends net.emaze.dysfunctional.convolutions.Zips { }
+    public abstract static class Counts extends net.emaze.dysfunctional.ranges.Counts { }
+    public abstract static class Consumers extends net.emaze.dysfunctional.consumers.Consumers { }
+    public abstract static class Filtering extends net.emaze.dysfunctional.filtering.Filtering { }
+    public abstract static class Iterations extends net.emaze.dysfunctional.iterations.Iterations { }
+    public abstract static class Comparing extends net.emaze.dysfunctional.order.Comparing { }
+    public abstract static class Strings extends net.emaze.dysfunctional.strings.Strings { }
+}
