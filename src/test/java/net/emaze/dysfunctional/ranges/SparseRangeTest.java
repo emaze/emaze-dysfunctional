f378e97cc15c3e34df3eab6aea305120ff237d6e
Roberto Ferranti <rf@emaze.net>
2010-12-17 12:50:04 +0100

    enh: coverage for SparseRange

--- src/test/java/net/emaze/dysfunctional/ranges/SparseRangeTest.java
+++ src/test/java/net/emaze/dysfunctional/ranges/SparseRangeTest.java
@@ -3,6 +3,7 @@ package net.emaze.dysfunctional.ranges;
 import java.lang.Integer;
 import java.util.Collections;
 import java.util.List;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -21,8 +22,45 @@ public class SparseRangeTest {
     public static class Functions {
 
         @Test
-        public void test() {
+        public void rangeIsNotEqualsToNull() {
+            SparseRange<Integer> sr = RangeMother.r(RangeMother.p(0,2), RangeMother.p(3, 5));
+            Assert.assertFalse(sr.equals(null));
         }
+        @Test
+        public void rangeIsEqualsToItSelf() {
+            SparseRange<Integer> sr = RangeMother.r(RangeMother.p(0,2), RangeMother.p(3, 5));
+            Assert.assertTrue(sr.equals(sr));
+        }
+        
+        @Test
+        public void sameRangesLeadsToSameHashCode() {
+            SparseRange<Integer> former = RangeMother.r(RangeMother.p(0,2), RangeMother.p(2, 3));
+            SparseRange<Integer> latter = RangeMother.r(RangeMother.p(0,3));
+            Assert.assertEquals(former.hashCode(), latter.hashCode());
+        }
+
+        @Test
+        public void containsElementIfAnyRangeContainsIt() {
+            SparseRange<Integer> sr = RangeMother.r(RangeMother.p(0,2), RangeMother.p(3, 5));
+            Assert.assertTrue(sr.contains(4));
+        }
+        
+        @Test
+        public void canIterateSparseRange() {
+            SparseRange<Integer> sr = RangeMother.r(RangeMother.p(0,2));
+            Assert.assertNotNull(sr.iterator());
+        }
+        
+        @Test
+        public void canCreateSparseRangeWithUnsortedDenseRanges() {
+            SparseRange<Integer> sr = new SparseRange<Integer>(RangeMother.sequencer, RangeMother.comparator, RangeMother.r(2,3), RangeMother.r(0, 1));
+        }
+        @Test
+        public void toStringShowsDensifiedRanges() {
+            SparseRange<Integer> sr = new SparseRange<Integer>(RangeMother.sequencer, RangeMother.comparator, RangeMother.r(0,1), RangeMother.r(1, 2), RangeMother.r(4,5));
+            Assert.assertEquals("[0-2,4-5]", sr.toString());
+        }
+        
     }
 
     public static class Degenerations {
