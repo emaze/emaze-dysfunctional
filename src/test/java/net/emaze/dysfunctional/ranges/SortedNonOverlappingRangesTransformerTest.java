b95bcfdf9f21d3edd7b7a8ba03d081437c5684f6
Roberto Ferranti <rf@emaze.net>
2010-12-17 14:07:55 +0100

    enh: SortedNonOverlappingRanges tests

--- src/test/java/net/emaze/dysfunctional/ranges/SortedNonOverlappingRangesTransformerTest.java
+++ src/test/java/net/emaze/dysfunctional/ranges/SortedNonOverlappingRangesTransformerTest.java
@@ -1,12 +1,8 @@
 package net.emaze.dysfunctional.ranges;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import junit.framework.Assert;
-import net.emaze.dysfunctional.order.ComparableComparator;
-import net.emaze.dysfunctional.order.IntegerSequencingPolicy;
-import net.emaze.dysfunctional.order.SequencingPolicy;
 import org.junit.Test;
 
 /**
@@ -14,14 +10,20 @@ import org.junit.Test;
  * @author rferranti
  */
 public class SortedNonOverlappingRangesTransformerTest {
-    private final SequencingPolicy<Integer> sequencer = new IntegerSequencingPolicy();
-    private final Comparator<Integer> comparator = new ComparableComparator<Integer>();
-    
+
     @Test
     public void canMergeTwoContiguousNonOverlappingRanges() {
-        DenseRange<Integer> a = new DenseRange<Integer>(sequencer, comparator, 0, 1);
-        DenseRange<Integer> b = new DenseRange<Integer>(sequencer, comparator, 2, 3);
-        List<DenseRange<Integer>> got = new SortedNonOverlappingRangesTransformer<Integer>(sequencer, comparator).perform(Arrays.asList(a,b));
+        DenseRange<Integer> a = new DenseRange<Integer>(RangeMother.sequencer, RangeMother.comparator, 1, 1);
+        DenseRange<Integer> b = new DenseRange<Integer>(RangeMother.sequencer, RangeMother.comparator, 2, 2);
+        List<DenseRange<Integer>> got = new SortedNonOverlappingRangesTransformer<Integer>(RangeMother.sequencer, RangeMother.comparator).perform(Arrays.asList(a, b));
         Assert.assertEquals(1, got.size());
+    }
+    
+    @Test
+    public void nonOverlappingRangesNonCountiguousRangesAreNotMerged() {
+        DenseRange<Integer> a = new DenseRange<Integer>(RangeMother.sequencer, RangeMother.comparator, 1, 1);
+        DenseRange<Integer> b = new DenseRange<Integer>(RangeMother.sequencer, RangeMother.comparator, 3, 3);
+        List<DenseRange<Integer>> got = new SortedNonOverlappingRangesTransformer<Integer>(RangeMother.sequencer, RangeMother.comparator).perform(Arrays.asList(a, b));
+        Assert.assertEquals(2, got.size());
     }
 }
