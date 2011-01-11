ebafbd59226aea1d28e3ae3d195fc33dfbfda685
Roberto Ferranti <roberto.ferranti@gmail.com>
2011-01-11 20:30:50 +0100

    enh: binary always

---
+++ src/main/java/net/emaze/dysfunctional/delegates/BinaryAlways.java
@@ -0,0 +1,21 @@
+package net.emaze.dysfunctional.delegates;
+
+/**
+ * a Null Binary Predicate always returning true
+ * @param <E1> the first type parameter
+ * @param <E2> the second type parameter
+ * @author rferranti
+ */
+public class BinaryAlways<E1, E2> implements BinaryPredicate<E1, E2> {
+
+    /**
+     * yields true.
+     * @param former the ignored element
+     * @param latter the ignored element
+     * @return true. always.
+     */
+    @Override
+    public boolean test(E1 former, E2 latter) {
+        return true;
+    }
+}
