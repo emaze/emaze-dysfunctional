a9ae9df76bd6391fc351c066b71771a775bf013e
Roberto Ferranti <rf@emaze.net>
2011-01-13 11:11:46 +0100

    ref: removed not implemented dbc methods

--- src/main/java/net/emaze/dysfunctional/contracts/dbc.java
+++ src/main/java/net/emaze/dysfunctional/contracts/dbc.java
@@ -6,24 +6,12 @@ package net.emaze.dysfunctional.contracts;
  */
 public abstract class dbc {
 
-    public static interface Statement{
-        public boolean evaluate(); //TODO: hamcrest? if so we should write an adapter to throw the right exception
-    }
-
-    public static void precondition(Statement statement){
-        precondition(statement.evaluate(), "precondition failed: %s", statement);
-    }
-
     public static void precondition(boolean assertion, String format, Object... params) {
         if (!assertion) {
             throw new IllegalArgumentException(String.format(format, params));
         }
     }
     
-    public static void stateprecondition(Statement statement){
-        stateprecondition(statement.evaluate(), "precondition (state) failed: %s", statement);
-    }
-
     public static void stateprecondition(boolean assertion, String format, Object... params) {
         if (!assertion) {
             throw new IllegalStateException(String.format(format, params));
