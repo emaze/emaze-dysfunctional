7ca80402a5c96bda12e1ed859d9cc82be26fa55c
Roberto Ferranti <rf@emaze.net>
2011-01-17 14:03:31 +0100

    ref: unused imports

--- src/test/java/net/emaze/dysfunctional/multiplexing/ChainIteratorTest.java
+++ src/test/java/net/emaze/dysfunctional/multiplexing/ChainIteratorTest.java
@@ -1,13 +1,10 @@
 package net.emaze.dysfunctional.multiplexing;
 
-import java.lang.Object;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
