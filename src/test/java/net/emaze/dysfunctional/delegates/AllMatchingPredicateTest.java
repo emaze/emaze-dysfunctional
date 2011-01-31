df96307cf5fad72d4e795ce8aa148def3f7937be
Roberto Ferranti <rf@emaze.net>
2011-01-31 17:04:55 +0100

    enh: all matching predicate test coverage

--- src/test/java/net/emaze/dysfunctional/delegates/AllMatchingPredicateTest.java
+++ src/test/java/net/emaze/dysfunctional/delegates/AllMatchingPredicateTest.java
@@ -1,14 +1,10 @@
-
 package net.emaze.dysfunctional.delegates;
 
-import java.lang.String;
+import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class AllMatchingPredicateTest {
-
-    public AllMatchingPredicateTest() {
-    }
 
     @Test
     public void canEvaluateEmptyPredicateList() {
@@ -29,4 +25,44 @@ public class AllMatchingPredicateTest {
         Assert.assertFalse(test.test("aaa"));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void addingNullFunctorToPredicateYieldsException() {
+        AllMatchingPredicate<String> pred = new AllMatchingPredicate<String>();
+        pred.add(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void removingNullFunctorToPredicateYieldsException() {
+        AllMatchingPredicate<String> pred = new AllMatchingPredicate<String>();
+        pred.remove(null);
+    }
+
+    @Test
+    public void removingNonPresentPredicateYieldsFalse() {
+        AllMatchingPredicate<String> pred = new AllMatchingPredicate<String>();
+        boolean got = pred.remove(new Always<String>());
+        Assert.assertFalse(got);
+    }
+
+    @Test
+    public void removingPresentPredicateYieldsTrue() {
+        AllMatchingPredicate<String> pred = new AllMatchingPredicate<String>();
+        Always<String> always = new Always<String>();
+        pred.add(always);
+        boolean got = pred.remove(always);
+        Assert.assertTrue(got);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void settingNullFunctorsCollectionYieldsException() {
+        AllMatchingPredicate<String> pred = new AllMatchingPredicate<String>();
+        pred.setFunctors(null);
+    }
+
+    @Test
+    public void canSetFunctors() {
+        AllMatchingPredicate<String> pred = new AllMatchingPredicate<String>();
+        pred.setFunctors(Arrays.<Predicate<String>>asList(new Always<String>(), new Always<String>()));
+        pred.test("ignored_value");
+    }
 }
