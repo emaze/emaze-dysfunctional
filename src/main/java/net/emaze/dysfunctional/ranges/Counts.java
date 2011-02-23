dc1b507595b07268c036410f6275c6cb9f6a7946
Roberto Ferranti <rf@emaze.net>
2011-02-23 14:57:18 +0100

    enh: counts facade methods

--- src/main/java/net/emaze/dysfunctional/ranges/Counts.java
+++ src/main/java/net/emaze/dysfunctional/ranges/Counts.java
@@ -1,6 +1,7 @@
 package net.emaze.dysfunctional.ranges;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import net.emaze.dysfunctional.contracts.dbc;
 import net.emaze.dysfunctional.iterations.OneTimeIterable;
 import net.emaze.dysfunctional.order.ComparableComparator;
@@ -14,15 +15,24 @@ import net.emaze.dysfunctional.tuples.Pair;
  */
 public abstract class Counts {
 
-    public static <T> Iterable<Pair<Integer, T>> counted(Iterable<T> iterable) {
+    public static <T1, T2> Iterable<Pair<T1, T2>> counted(Iterator<T2> iterator, Range<T1> range) {
+        return new OneTimeIterable<Pair<T1, T2>>(new CountedIterator<T1, T2>(iterator, range));
+    }
+
+    public static <T> Iterable<Pair<Integer, T>> counted(Iterator<T> iterator) {
         final SequencingPolicy<Integer> sequencer = new IntegerSequencingPolicy();
         final Comparator<Integer> comparator = new ComparableComparator<Integer>();
         final Range<Integer> range = new DenseRange<Integer>(sequencer, comparator, 0, Integer.MAX_VALUE);
-        return counted(iterable, range);
+        return counted(iterator, range);
+    }
+
+    public static <T> Iterable<Pair<Integer, T>> counted(Iterable<T> iterable) {
+        dbc.precondition(iterable != null, "cannot call counted with a null iterable");
+        return counted(iterable.iterator());
     }
 
     public static <T1, T2> Iterable<Pair<T1, T2>> counted(Iterable<T2> iterable, Range<T1> range) {
         dbc.precondition(iterable != null, "cannot call counted with a null iterable");
-        return new OneTimeIterable<Pair<T1, T2>>(new CountedIterator<T1, T2>(iterable.iterator(), range));
+        return counted(iterable.iterator(), range);
     }
 }
