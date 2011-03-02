bc7c83fd88285061f4176f9ceb7e4ba6424fadaa
Roberto Ferranti <rf@emaze.net>
2011-03-02 17:39:34 +0100

    enh: IsContainedIn predicate

---
+++ src/main/java/net/emaze/dysfunctional/logic/IsContainedIn.java
@@ -0,0 +1,23 @@
+package net.emaze.dysfunctional.logic;
+
+import java.util.Collection;
+import net.emaze.dysfunctional.contracts.dbc;
+
+/**
+ *
+ * @author rferranti
+ */
+public class IsContainedIn<T> implements Predicate<T> {
+
+    private final Collection<T> collection;
+
+    public IsContainedIn(Collection<T> collection) {
+        dbc.precondition(collection != null, "cannot create IsContainedIn predicate with a null collection");
+        this.collection = collection;
+    }
+
+    @Override
+    public boolean test(T element) {
+        return collection.contains(element);
+    }
+}
