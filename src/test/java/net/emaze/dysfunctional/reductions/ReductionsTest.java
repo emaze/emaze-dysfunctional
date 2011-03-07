52058a023f15e750d273820fdbd171ac6deab5c8
Roberto Ferranti <rf@emaze.net>
2011-03-07 09:50:34 +0100

    enh: coverage

--- src/test/java/net/emaze/dysfunctional/reductions/ReductionsTest.java
+++ src/test/java/net/emaze/dysfunctional/reductions/ReductionsTest.java
@@ -2,63 +2,110 @@ package net.emaze.dysfunctional.reductions;
 
 import java.util.Arrays;
 import java.util.List;
+import net.emaze.dysfunctional.order.ComparableComparator;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  *
  * @author rferranti
  */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ReductionsTest.Reduce.class,
+    ReductionsTest.CountLong.class,
+    ReductionsTest.CountInteger.class,
+    ReductionsTest.Maximum.class,
+    ReductionsTest.Minimum.class
+})
 public class ReductionsTest {
 
-    private List<Integer> list = Arrays.asList(1, 2);
-    private Integer[] array = new Integer[]{1, 2};
+    private static List<Integer> list = Arrays.asList(1, 2);
+    private static Integer[] array = new Integer[]{1, 2};
 
-    @Test
-    public void canCountFromIterator() {
-        Assert.assertEquals(2l, Reductions.count(list.iterator()));
+    public static class Reduce {
+
+        @Test
+        public void canReduceFromIterator() {
+            Assert.assertEquals(Long.valueOf(2l), Reductions.reduce(list.iterator(), new Count<Integer>(), 0l));
+        }
+
+        @Test
+        public void canReduceFromIterable() {
+            Assert.assertEquals(Long.valueOf(2l), Reductions.reduce(list, new Count<Integer>(), 0l));
+        }
+
+        @Test
+        public void canReduceFromArray() {
+            Assert.assertEquals(Long.valueOf(2l), Reductions.reduce(array, new Count<Integer>(), 0l));
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotCallReduceWithNullIterable() {
+            final Iterable<Object> iterable = null;
+            Reductions.reduce(iterable, new Count<Object>(), 0L);
+        }
     }
 
-    @Test
-    public void canCountFromIterable() {
-        Assert.assertEquals(2l, Reductions.count(list));
+    public static class CountLong {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotCallCountWithNullIterable() {
+            final Iterable<Object> iterable = null;
+            Reductions.count(iterable);
+        }
+
+        @Test
+        public void canCountFromIterator() {
+            Assert.assertEquals(2l, Reductions.count(list.iterator()));
+        }
+
+        @Test
+        public void canCountFromIterable() {
+            Assert.assertEquals(2l, Reductions.count(list));
+        }
     }
 
-    @Test
-    public void canCountAsIntegerFromIterator() {
-        Assert.assertEquals(2l, Reductions.counti(list.iterator()));
+    public static class CountInteger {
+
+        @Test
+        public void canCountAsIntegerFromIterator() {
+            Assert.assertEquals(2l, Reductions.counti(list.iterator()));
+        }
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotCallCountiWithNullIterable() {
+            final Iterable<Object> iterable = null;
+            Reductions.counti(iterable);
+        }
     }
 
-    @Test
-    public void canReduceFromIterator() {
-        Assert.assertEquals(Long.valueOf(2l), Reductions.reduce(list.iterator(), new Count<Integer>(), 0l));
+    public static class Maximum {
+
+        @Test
+        public void canExtractMaximumWithComparator() {
+            int max = Reductions.max(list.iterator(), new ComparableComparator<Integer>(), 0);
+            Assert.assertEquals(2, max);
+        }
+        @Test
+        public void canExtractMaximumWithComparable() {
+            int max = Reductions.max(list.iterator(), 0);
+            Assert.assertEquals(2, max);
+        }
     }
 
-    @Test
-    public void canReduceFromIterable() {
-        Assert.assertEquals(Long.valueOf(2l), Reductions.reduce(list, new Count<Integer>(), 0l));
-    }
-
-    @Test
-    public void canReduceFromArray() {
-        Assert.assertEquals(Long.valueOf(2l), Reductions.reduce(array, new Count<Integer>(), 0l));
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotCallReduceWithNullIterable() {
-        final Iterable<Object> iterable = null;
-        Reductions.reduce(iterable, new Count<Object>(), 0L);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotCallCountWithNullIterable() {
-        final Iterable<Object> iterable = null;
-        Reductions.count(iterable);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotCallCountiWithNullIterable() {
-        final Iterable<Object> iterable = null;
-        Reductions.counti(iterable);
+    public static class Minimum {
+        @Test
+        public void canExtractMinimumWithComparator() {
+            int min = Reductions.min(list.iterator(), new ComparableComparator<Integer>(), 2);
+            Assert.assertEquals(1, min);
+        }
+        @Test
+        public void canExtractMinimumWithComparable() {
+            int min = Reductions.min(list.iterator(), 2);
+            Assert.assertEquals(1, min);
+        }
     }
 }
