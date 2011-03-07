a9dced37a0fff73beb6d5d26f5f497b12dae4c16
Roberto Ferranti <rf@emaze.net>
2011-03-07 17:11:18 +0100

    enh: collection adder

---
+++ src/main/java/net/emaze/dysfunctional/collections/CollectionAdder.java
@@ -0,0 +1,25 @@
+package net.emaze.dysfunctional.collections;
+
+import java.util.Collection;
+import net.emaze.dysfunctional.contracts.dbc;
+import net.emaze.dysfunctional.delegates.Delegate;
+
+/**
+ *
+ * @author rferranti
+ */
+public class CollectionAdder<T> implements Delegate<T, T> {
+
+    private final Collection<T> collection;
+
+    public CollectionAdder(Collection<T> collection) {
+        dbc.precondition(collection != null, "cannot create a CollectionAdder with a null collection");
+        this.collection = collection;
+    }
+
+    @Override
+    public T perform(T element) {
+        collection.add(element);
+        return element;
+    }
+}
---
+++ src/test/java/net/emaze/dysfunctional/collections/CollectionAdderTest.java
@@ -0,0 +1,26 @@
+package net.emaze.dysfunctional.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import net.emaze.dysfunctional.testing.O;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author rferranti
+ */
+public class CollectionAdderTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingCollecitonAdderWithNullCollectionYieldsException() {
+        new CollectionAdder<O>(null);
+    }
+
+    @Test
+    public void canAddToCollection() {
+        final List<O> bucket = new ArrayList<O>();
+        new CollectionAdder<O>(bucket).perform(O.ONE);
+        Assert.assertEquals(1, bucket.size());
+    }
+}
