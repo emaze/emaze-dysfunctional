30067520edbb617e2ce9fb3a2f66bc318c8807a1
Roberto Ferranti <rf@emaze.net>
2011-03-07 17:21:08 +0100

    enh: collectionAllAdder implementation

---
+++ src/main/java/net/emaze/dysfunctional/collections/CollectionAllAdder.java
@@ -0,0 +1,27 @@
+package net.emaze.dysfunctional.collections;
+
+import java.util.Collection;
+import net.emaze.dysfunctional.contracts.dbc;
+import net.emaze.dysfunctional.delegates.Delegate;
+
+/**
+ *
+ * @param <C> the collection type parameter
+ * @param <E> the element type parameter
+ * @author rferranti
+ */
+public class CollectionAllAdder<C extends Collection<E>, E> implements Delegate<C, C> {
+
+    private final Collection<E> collection;
+
+    public CollectionAllAdder(Collection<E> collection) {
+        dbc.precondition(collection != null, "cannot create a CollectionAllAdder with a null collection");
+        this.collection = collection;
+    }
+
+    @Override
+    public C perform(C elements) {
+        collection.addAll(elements);
+        return elements;
+    }
+}
