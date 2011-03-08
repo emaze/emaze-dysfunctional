04f27dee20624ffef4dcc866cebd1dd518118055
Roberto Ferranti <rf@emaze.net>
2011-03-08 18:36:52 +0100

    enhy: coverage

--- src/test/java/net/emaze/dysfunctional/multiplexing/MultiplexingTest.java
+++ src/test/java/net/emaze/dysfunctional/multiplexing/MultiplexingTest.java
@@ -1,8 +1,11 @@
 package net.emaze.dysfunctional.multiplexing;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import net.emaze.dysfunctional.options.MaybeIterator;
 import net.emaze.dysfunctional.testing.O;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -23,12 +26,33 @@ import org.junit.runners.Suite;
 })
 public class MultiplexingTest {
 
+    public static List<O> AN_ITERABLE = Arrays.asList(O.ONE);
+
     public static class Chain {
 
         @Test(expected = IllegalArgumentException.class)
         public void cannotChainNullIterable() {
             final Iterable<Iterator<O>> iterable = null;
             Multiplexing.chain(iterable);
+        }
+
+        @Test
+        public void canChainFromIterable() {
+            final List<Iterator<O>> iterable = Arrays.asList(AN_ITERABLE.iterator());
+            final Iterator<O> chained = Multiplexing.chain(iterable);
+            Assert.assertNotNull(chained);
+        }
+        
+        @Test
+        public void canChainFromIterator() {
+            final List<Iterator<O>> iterable = Arrays.asList(AN_ITERABLE.iterator());
+            final Iterator<O> chained = Multiplexing.chain(iterable.iterator());
+            Assert.assertNotNull(chained);
+        }
+        @Test
+        public void canChainFromArray() {
+            final Iterator<O> chained = Multiplexing.chain(AN_ITERABLE.iterator(), AN_ITERABLE.iterator());
+            Assert.assertNotNull(chained);
         }
     }
 
@@ -38,6 +62,25 @@ public class MultiplexingTest {
         public void cannotFlatternANullIterable() {
             final Iterable<Iterable<O>> iterable = null;
             Multiplexing.flatten(iterable);
+        }
+
+        @Test
+        public void canFlattenFromIterable() {
+            final List<Iterable<O>> iterable = Arrays.<Iterable<O>>asList(AN_ITERABLE);
+            final Iterator<O> flattened = Multiplexing.flatten(iterable);
+            Assert.assertNotNull(flattened);
+        }
+
+        @Test
+        public void canFlattenFromIterator() {
+            final List<Iterable<O>> iterable = Arrays.<Iterable<O>>asList(AN_ITERABLE);
+            final Iterator<O> flattened = Multiplexing.flatten(iterable.iterator());
+            Assert.assertNotNull(flattened);
+        }
+        @Test
+        public void canFlattenFromArray() {
+            final Iterator<O> flattened = Multiplexing.flatten(AN_ITERABLE, AN_ITERABLE);
+            Assert.assertNotNull(flattened);
         }
     }
 
