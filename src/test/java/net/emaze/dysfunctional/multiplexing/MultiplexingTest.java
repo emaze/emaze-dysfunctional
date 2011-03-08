4ca67c6bb617845b92f3e218748390f3d2013f4e
Roberto Ferranti <rf@emaze.net>
2011-03-08 18:25:27 +0100

    fix: unintended method calls with untyped generics in tests

--- src/test/java/net/emaze/dysfunctional/multiplexing/MultiplexingTest.java
+++ src/test/java/net/emaze/dysfunctional/multiplexing/MultiplexingTest.java
@@ -1,53 +1,86 @@
 package net.emaze.dysfunctional.multiplexing;
 
+import java.util.Iterator;
+import net.emaze.dysfunctional.options.MaybeIterator;
+import net.emaze.dysfunctional.testing.O;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  *
- * @author dangelocola
+ * @author dangelocola, rferranti
  */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    MultiplexingTest.Chain.class,
+    MultiplexingTest.Flatten.class,
+    MultiplexingTest.Mux.class,
+    MultiplexingTest.Demux.class,
+    MultiplexingTest.Roundrobin.class
+})
 public class MultiplexingTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotFlatternANullIterable() {
-        final Iterable<Object> iterable = null;
-        Multiplexing.flatten(iterable);
+    public static class Chain {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotChainNullIterable() {
+            final Iterable<Iterator<O>> iterable = null;
+            Multiplexing.chain(iterable);
+        }
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotChaingANullIterable() {
-        final Iterable iterable = null;
-        Multiplexing.chain(iterable);
+    public static class Flatten {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotFlatternANullIterable() {
+            final Iterable<Iterable<O>> iterable = null;
+            Multiplexing.flatten(iterable);
+        }
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotMuxANullIterable() {
-        final Iterable iterable = null;
-        Multiplexing.mux(iterable);
+    public static class Mux {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotMuxANullIterable() {
+            final Iterable<Iterator<O>> iterable = null;
+            Multiplexing.mux(iterable);
+        }
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotMuxlANullIterable() {
-        final Iterable iterable = null;
-        Multiplexing.muxl(iterable);
+    public static class Muxl {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotMuxlANullIterable() {
+            final Iterable<Iterator<O>> iterable = null;
+            Multiplexing.muxl(iterable);
+        }
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotDemuxANullIterable() {
-        final Iterable iterable = null;
-        Multiplexing.demux(1, iterable);
+    public static class Demux {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotDemuxANullIterable() {
+            final Iterable<Iterator<O>> iterable = null;
+            Multiplexing.demux(1, iterable);
+        }
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotDemuxlANullIterable() {
-        final Iterable iterable = null;
-        Multiplexing.demuxl(1, iterable);
+    public static class Demuxl {
+
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotDemuxlANullIterable() {
+            final MaybeIterator<O> iterable = null;
+            Multiplexing.demuxl(1, iterable);
+        }
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void cannotRoundRobinANullIterable() {
-        final Iterable iterable = null;
-        Multiplexing.roundrobin(iterable);
-    }
+    public static class Roundrobin {
 
+        @Test(expected = IllegalArgumentException.class)
+        public void cannotRoundRobinANullIterable() {
+            final Iterable<Iterator<O>> iterable = null;
+            Multiplexing.roundrobin(iterable);
+        }
+    }
 }
