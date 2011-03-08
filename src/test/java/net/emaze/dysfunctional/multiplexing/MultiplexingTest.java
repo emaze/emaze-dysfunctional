cc7e9da8926f253d0bbf84feb5611c442d303b13
Roberto Ferranti <rf@emaze.net>
2011-03-08 18:51:22 +0100

    enh: muxl/demuxl coverage

--- src/test/java/net/emaze/dysfunctional/multiplexing/MultiplexingTest.java
+++ src/test/java/net/emaze/dysfunctional/multiplexing/MultiplexingTest.java
@@ -3,6 +3,7 @@ package net.emaze.dysfunctional.multiplexing;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import net.emaze.dysfunctional.iterations.Iterations;
 import net.emaze.dysfunctional.options.Maybe;
 import net.emaze.dysfunctional.options.MaybeIterator;
 import net.emaze.dysfunctional.testing.O;
@@ -43,13 +44,14 @@ public class MultiplexingTest {
             final Iterator<O> chained = Multiplexing.chain(iterable);
             Assert.assertNotNull(chained);
         }
-        
+
         @Test
         public void canChainFromIterator() {
             final List<Iterator<O>> iterable = Arrays.asList(AN_ITERABLE.iterator());
             final Iterator<O> chained = Multiplexing.chain(iterable.iterator());
             Assert.assertNotNull(chained);
         }
+
         @Test
         public void canChainFromArray() {
             final Iterator<O> chained = Multiplexing.chain(AN_ITERABLE.iterator(), AN_ITERABLE.iterator());
@@ -78,6 +80,7 @@ public class MultiplexingTest {
             final Iterator<O> flattened = Multiplexing.flatten(iterable.iterator());
             Assert.assertNotNull(flattened);
         }
+
         @Test
         public void canFlattenFromArray() {
             final Iterator<O> flattened = Multiplexing.flatten(AN_ITERABLE, AN_ITERABLE);
@@ -106,6 +109,7 @@ public class MultiplexingTest {
             final Iterator<O> muxed = Multiplexing.mux(iterable.iterator());
             Assert.assertNotNull(muxed);
         }
+
         @Test
         public void canMuxFromArray() {
             final Iterator<O> muxed = Multiplexing.mux(AN_ITERABLE.iterator(), AN_ITERABLE.iterator());
@@ -120,6 +124,7 @@ public class MultiplexingTest {
             final Iterable<Iterator<O>> iterable = null;
             Multiplexing.muxl(iterable);
         }
+
         @Test
         public void canMuxLongestFromIterable() {
             final List<Iterator<O>> iterable = Arrays.asList(AN_ITERABLE.iterator());
@@ -133,6 +138,7 @@ public class MultiplexingTest {
             final Iterator<Maybe<O>> muxed = Multiplexing.muxl(iterable.iterator());
             Assert.assertNotNull(muxed);
         }
+
         @Test
         public void canMuxLongestFromArray() {
             final Iterator<Maybe<O>> muxed = Multiplexing.muxl(AN_ITERABLE.iterator(), AN_ITERABLE.iterator());
@@ -147,6 +153,24 @@ public class MultiplexingTest {
             final Iterable<Iterator<O>> iterable = null;
             Multiplexing.demux(1, iterable);
         }
+
+        @Test
+        public void canDemuxFromIterable() {
+            Iterator<List<O>> demux = Multiplexing.demux(1, AN_ITERABLE);
+            Assert.assertNotNull(demux);
+        }
+
+        @Test
+        public void canDemuxFromIterator() {
+            Iterator<List<O>> demux = Multiplexing.demux(1, AN_ITERABLE.iterator());
+            Assert.assertNotNull(demux);
+        }
+
+        @Test
+        public void canDemuxFromArray() {
+            Iterator<List<O>> demux = Multiplexing.demux(1, O.IGNORED, O.IGNORED, O.IGNORED);
+            Assert.assertNotNull(demux);
+        }
     }
 
     public static class Demuxl {
@@ -155,6 +179,25 @@ public class MultiplexingTest {
         public void cannotDemuxlANullIterable() {
             final MaybeIterator<O> iterable = null;
             Multiplexing.demuxl(1, iterable);
+        }
+
+        @Test
+        public void canDemuxLongestFromIterable() {
+            MaybeIterator<O> maybeIter = new MaybeIterator<O>(AN_ITERABLE.iterator());
+            Iterator<List<Maybe<O>>> demuxl = Multiplexing.demuxl(1, Iterations.oneTime(maybeIter));
+            Assert.assertNotNull(demuxl);
+        }
+
+        @Test
+        public void canDemuxLongestFromIterator() {
+            Iterator<List<Maybe<O>>> demuxl = Multiplexing.demuxl(1, new MaybeIterator<O>(AN_ITERABLE.iterator()));
+            Assert.assertNotNull(demuxl);
+        }
+
+        @Test
+        public void canDemuxLongestFromArray() {
+            Iterator<List<Maybe<O>>> demuxl = Multiplexing.demuxl(1, Maybe.just(O.IGNORED), Maybe.<O>nothing());
+            Assert.assertNotNull(demuxl);
         }
     }
 
