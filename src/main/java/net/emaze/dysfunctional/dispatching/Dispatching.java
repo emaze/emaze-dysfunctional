217af87936b0f6b6269be410fa399edd3715fb40
Roberto Ferranti <rf@emaze.net>
2011-03-25 19:29:19 +0100

    enh/ref: curry/rcurry/mcurry/delegate/ignore

--- src/main/java/net/emaze/dysfunctional/dispatching/Dispatching.java
+++ src/main/java/net/emaze/dysfunctional/dispatching/Dispatching.java
@@ -7,6 +7,16 @@ import net.emaze.dysfunctional.dispatching.delegates.Composer;
 import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import net.emaze.dysfunctional.dispatching.delegates.Provider;
 import net.emaze.dysfunctional.dispatching.actions.TernaryAction;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionBinderFirst;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionBinderFirstOfThree;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionBinderSecond;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionBinderSecondOfThree;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionBinderThird;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionIgnoreFirst;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionIgnoreFirstOfThree;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionIgnoreSecond;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionIgnoreSecondOfThree;
+import net.emaze.dysfunctional.dispatching.actions.adapting.ActionIgnoreThird;
 import net.emaze.dysfunctional.dispatching.delegates.TernaryDelegate;
 import net.emaze.dysfunctional.dispatching.delegates.adapting.Binder;
 import net.emaze.dysfunctional.dispatching.delegates.adapting.BinderFirst;
@@ -14,11 +24,24 @@ import net.emaze.dysfunctional.dispatching.delegates.adapting.BinderFirstOfThree
 import net.emaze.dysfunctional.dispatching.delegates.adapting.BinderSecond;
 import net.emaze.dysfunctional.dispatching.delegates.adapting.BinderSecondOfThree;
 import net.emaze.dysfunctional.dispatching.delegates.adapting.BinderThird;
+import net.emaze.dysfunctional.dispatching.delegates.adapting.IgnoreFirst;
+import net.emaze.dysfunctional.dispatching.delegates.adapting.IgnoreFirstOfThree;
+import net.emaze.dysfunctional.dispatching.delegates.adapting.IgnoreSecond;
+import net.emaze.dysfunctional.dispatching.delegates.adapting.IgnoreSecondOfThree;
+import net.emaze.dysfunctional.dispatching.delegates.adapting.IgnoreThird;
+import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateBinderFirst;
 import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateBinderSecond;
+import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateBinderSecondOfThree;
 import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateIgnoreSecond;
 import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateIgnoreFirst;
+import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateIgnoreFirstOfThree;
 import net.emaze.dysfunctional.dispatching.logic.BinaryPredicate;
 import net.emaze.dysfunctional.dispatching.logic.Predicate;
+import net.emaze.dysfunctional.dispatching.logic.TernaryPredicate;
+import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateBinderFirstOfThree;
+import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateBinderThird;
+import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateIgnoreSecondOfThree;
+import net.emaze.dysfunctional.dispatching.logic.adapters.PredicateIgnoreThird;
 
 /**
  *
@@ -26,129 +49,164 @@ import net.emaze.dysfunctional.dispatching.logic.Predicate;
  */
 public abstract class Dispatching {
 
-    /**
-     * 
-     * @param <T>
-     * @param adaptee
-     * @return
-     */
-    public static <T> ActionToDelegate<T> action2delegate(Action<T> adaptee) {
-        return new ActionToDelegate<T>(adaptee);
+    public static <T1, T2> Action<T2> curry(BinaryAction<T1, T2> action, T1 first) {
+        return new ActionBinderFirst<T1, T2>(action, first);
     }
 
-    /**
-     * 
-     * @param <T1>
-     * @param <T2>
-     * @param adaptee
-     * @return
-     */
-    public static <T1, T2> BinaryActionToBinaryDelegate<T1, T2> action2delegate(BinaryAction<T1, T2> adaptee) {
-        return new BinaryActionToBinaryDelegate<T1, T2>(adaptee);
+    public static <T1, T2, T3> BinaryAction<T2, T3> curry(TernaryAction<T1, T2, T3> action, T1 first) {
+        return new ActionBinderFirstOfThree<T1, T2, T3>(action, first);
     }
 
-    /**
-     * 
-     * @param <T1>
-     * @param <T2>
-     * @param <T3>
-     * @param adaptee
-     * @return
-     */
-    public static <T1, T2, T3> TernaryActionToTernaryDelegate<T1, T2, T3> action2delegate(TernaryAction<T1, T2, T3> adaptee) {
-        return new TernaryActionToTernaryDelegate<T1, T2, T3>(adaptee);
+    public static <T1, T2> Predicate<T2> curry(BinaryPredicate<T1, T2> predicate, T1 first) {
+        return new PredicateBinderFirst<T1, T2>(predicate, first);
     }
 
-    /**
-     * 
-     * @param <R>
-     * @param <T>
-     * @param delegate
-     * @param only
-     * @return
-     */
-    public static <R, T> Provider<R> bind(Delegate<R, T> delegate, T only) {
+    public static <T1, T2, T3> BinaryPredicate<T2, T3> curry(TernaryPredicate<T1, T2, T3> predicate, T1 first) {
+        return new PredicateBinderFirstOfThree<T1, T2, T3>(predicate, first);
+    }
+
+    public static <R, T> Provider<R> curry(Delegate<R, T> delegate, T only) {
         return new Binder<R, T>(delegate, only);
     }
 
-    /**
-     *
-     * @param <R>
-     * @param <T1>
-     * @param <T2>
-     * @param delegate
-     * @param first
-     * @return
-     */
-    public static <R, T1, T2> Delegate<R, T2> bind1st(BinaryDelegate<R, T1, T2> delegate, T1 first) {
+    public static <R, T1, T2> Delegate<R, T2> curry(BinaryDelegate<R, T1, T2> delegate, T1 first) {
         return new BinderFirst<R, T1, T2>(delegate, first);
     }
 
-    /**
-     * 
-     * @param <R>
-     * @param <T1>
-     * @param <T2>
-     * @param <T3>
-     * @param delegate
-     * @param first
-     * @return
-     */
-    public static <R, T1, T2, T3> BinaryDelegate<R, T2, T3> bind1st(TernaryDelegate<R, T1, T2, T3> delegate, T1 first) {
+    public static <R, T1, T2, T3> BinaryDelegate<R, T2, T3> curry(TernaryDelegate<R, T1, T2, T3> delegate, T1 first) {
         return new BinderFirstOfThree<R, T1, T2, T3>(delegate, first);
     }
 
-    /**
-     * 
-     * @param <R>
-     * @param <T1>
-     * @param <T2>
-     * @param delegate
-     * @param second
-     * @return
-     */
-    public static <R, T1, T2> Delegate<R, T1> bind2nd(BinaryDelegate<R, T1, T2> delegate, T2 second) {
+    public static <T1, T2> Action<T1> mcurry(BinaryAction<T1, T2> action, T2 second) {
+        return new ActionBinderSecond<T1, T2>(action, second);
+    }
+
+    public static <T1, T2, T3> BinaryAction<T1, T3> mcurry(TernaryAction<T1, T2, T3> action, T2 first) {
+        return new ActionBinderSecondOfThree<T1, T2, T3>(action, first);
+    }
+
+    public static <R, T1, T2> Delegate<R, T1> mcurry(BinaryDelegate<R, T1, T2> delegate, T2 second) {
         return new BinderSecond<R, T1, T2>(delegate, second);
     }
 
-    public static <T1, T2> Predicate<T1> bind2nd(BinaryPredicate<T1, T2> predicate, T2 second) {
+    public static <R, T1, T2, T3> BinaryDelegate<R, T1, T3> mcurry(TernaryDelegate<R, T1, T2, T3> delegate, T2 first) {
+        return new BinderSecondOfThree<R, T1, T2, T3>(delegate, first);
+    }
+
+    public static <T1, T2> Predicate<T1> mcurry(BinaryPredicate<T1, T2> predicate, T2 second) {
         return new PredicateBinderSecond<T1, T2>(predicate, second);
     }
 
-    public static <T1, T2> BinaryPredicate<T1, T2> ignore2nd(Predicate<T1> predicate, Class<T2> ignored) {
-        return new PredicateIgnoreSecond<T1, T2>(predicate);
+    public static <T1, T2, T3> BinaryPredicate<T1, T3> mcurry(TernaryPredicate<T1, T2, T3> predicate, T2 second) {
+        return new PredicateBinderSecondOfThree<T1, T2, T3>(predicate, second);
+    }
+
+    public static <T1, T2> Action<T1> rcurry(BinaryAction<T1, T2> action, T2 second) {
+        return new ActionBinderSecond<T1, T2>(action, second);
+    }
+
+    public static <T1, T2, T3> BinaryAction<T1, T2> rcurry(TernaryAction<T1, T2, T3> action, T3 third) {
+        return new ActionBinderThird<T1, T2, T3>(action, third);
+    }
+
+    public static <T1, T2> Predicate<T1> rcurry(BinaryPredicate<T1, T2> predicate, T2 second) {
+        return new PredicateBinderSecond<T1, T2>(predicate, second);
+    }
+
+    public static <T1, T2, T3> BinaryPredicate<T1, T2> rcurry(TernaryPredicate<T1, T2, T3> predicate, T3 third) {
+        return new PredicateBinderThird<T1, T2, T3>(predicate, third);
+    }
+
+    public static <R, T1, T2> Delegate<R, T1> rcurry(BinaryDelegate<R, T1, T2> delegate, T2 second) {
+        return new BinderSecond<R, T1, T2>(delegate, second);
+    }
+
+    public static <R, T1, T2, T3> BinaryDelegate<R, T1, T2> rcurry(TernaryDelegate<R, T1, T2, T3> delegate, T3 third) {
+        return new BinderThird<R, T1, T2, T3>(delegate, third);
+    }
+
+    public static <T> ActionToDelegate<T> delegate(Action<T> adaptee) {
+        return new ActionToDelegate<T>(adaptee);
+    }
+
+    public static <T1, T2> BinaryActionToBinaryDelegate<T1, T2> delegate(BinaryAction<T1, T2> adaptee) {
+        return new BinaryActionToBinaryDelegate<T1, T2>(adaptee);
+    }
+
+    public static <T1, T2, T3> TernaryActionToTernaryDelegate<T1, T2, T3> delegate(TernaryAction<T1, T2, T3> adaptee) {
+        return new TernaryActionToTernaryDelegate<T1, T2, T3>(adaptee);
+    }
+
+    public static <T> PredicateToDelegate<T> delegate(Predicate<T> adaptee) {
+        return new PredicateToDelegate<T>(adaptee);
+    }
+
+    public static <T1, T2> BinaryPredicateToBinaryDelegate<T1, T2> delegate(BinaryPredicate<T1, T2> adaptee) {
+        return new BinaryPredicateToBinaryDelegate<T1, T2>(adaptee);
+    }
+
+    public static <T1, T2, T3> TernaryPredicateToTernaryDelegate<T1, T2, T3> delegate(TernaryPredicate<T1, T2, T3> adaptee) {
+        return new TernaryPredicateToTernaryDelegate<T1, T2, T3>(adaptee);
     }
 
     public static <T1, T2> BinaryPredicate<T1, T2> ignore1st(Predicate<T2> predicate, Class<T1> ignored) {
         return new PredicateIgnoreFirst<T1, T2>(predicate);
     }
 
-    /**
-     *
-     * @param <R>
-     * @param <T1>
-     * @param <T2>
-     * @param <T3>
-     * @param delegate
-     * @param first
-     * @return
-     */
-    public static <R, T1, T2, T3> BinaryDelegate<R, T1, T3> bind2nd(TernaryDelegate<R, T1, T2, T3> delegate, T2 first) {
-        return new BinderSecondOfThree<R, T1, T2, T3>(delegate, first);
+    public static <T1, T2, T3> TernaryPredicate<T1, T2, T3> ignore1st(BinaryPredicate<T2, T3> predicate, Class<T1> ignored) {
+        return new PredicateIgnoreFirstOfThree<T1, T2, T3>(predicate);
     }
 
-    /**
-     * 
-     * @param <R>
-     * @param <T1>
-     * @param <T2>
-     * @param <T3>
-     * @param delegate
-     * @param first
-     * @return
-     */
-    public static <R, T1, T2, T3> BinaryDelegate<R, T1, T2> bind3rd(TernaryDelegate<R, T1, T2, T3> delegate, T3 first) {
-        return new BinderThird<R, T1, T2, T3>(delegate, first);
+    public static <T1, T2> BinaryPredicate<T1, T2> ignore2nd(Predicate<T1> predicate, Class<T2> ignored) {
+        return new PredicateIgnoreSecond<T1, T2>(predicate);
+    }
+
+    public static <T1, T2, T3> TernaryPredicate<T1, T2, T3> ignore2nd(BinaryPredicate<T1, T3> predicate, Class<T2> ignored) {
+        return new PredicateIgnoreSecondOfThree<T1, T2, T3>(predicate);
+    }
+
+    public static <T1, T2, T3> TernaryPredicate<T1, T2, T3> ignore3rd(BinaryPredicate<T1, T2> predicate, Class<T3> ignored) {
+        return new PredicateIgnoreThird<T1, T2, T3>(predicate);
+    }
+
+    public static <T1, T2> BinaryAction<T1, T2> ignore1st(Action<T2> action, Class<T1> ignored) {
+        return new ActionIgnoreFirst<T1, T2>(action);
+    }
+
+    public static <T1, T2, T3> TernaryAction<T1, T2, T3> ignore1st(BinaryAction<T2, T3> action, Class<T1> ignored) {
+        return new ActionIgnoreFirstOfThree<T1, T2, T3>(action);
+    }
+
+    public static <T1, T2> BinaryAction<T1, T2> ignore2nd(Action<T1> action, Class<T2> ignored) {
+        return new ActionIgnoreSecond<T1, T2>(action);
+    }
+
+    public static <T1, T2, T3> TernaryAction<T1, T2, T3> ignore2nd(BinaryAction<T1, T3> action, Class<T2> ignored) {
+        return new ActionIgnoreSecondOfThree<T1, T2, T3>(action);
+    }
+
+    public static <T1, T2, T3> TernaryAction<T1, T2, T3> ignore3rd(BinaryAction<T1, T2> action, Class<T3> ignored) {
+        return new ActionIgnoreThird<T1, T2, T3>(action);
+    }
+
+    public static <R, T1, T2> BinaryDelegate<R, T1, T2> ignore1st(Delegate<R, T2> delegate, Class<T1> ignored) {
+        return new IgnoreFirst<R, T1, T2>(delegate);
+    }
+
+    public static <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> ignore1st(BinaryDelegate<R, T2, T3> delegate, Class<T1> ignored) {
+        return new IgnoreFirstOfThree<R, T1, T2, T3>(delegate);
+    }
+
+    public static <R, T1, T2> BinaryDelegate<R, T1, T2> ignore2nd(Delegate<R, T1> delegate, Class<T2> ignored) {
+        return new IgnoreSecond<R, T1, T2>(delegate);
+    }
+
+    public static <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> ignore2nd(BinaryDelegate<R, T1, T3> delegate, Class<T2> ignored) {
+        return new IgnoreSecondOfThree<R, T1, T2, T3>(delegate);
+    }
+
+    public static <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> ignore3rd(BinaryDelegate<R, T1, T2> delegate, Class<T3> ignored) {
+        return new IgnoreThird<R, T1, T2, T3>(delegate);
     }
 
     /**
--- src/test/java/net/emaze/dysfunctional/dispatching/DispatchingTest.java
+++ src/test/java/net/emaze/dysfunctional/dispatching/DispatchingTest.java
@@ -8,7 +8,6 @@ import net.emaze.dysfunctional.dispatching.delegates.Identity;
 import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import net.emaze.dysfunctional.dispatching.delegates.BinaryDelegate;
 import net.emaze.dysfunctional.dispatching.delegates.TernaryDelegate;
-import net.emaze.dysfunctional.dispatching.Dispatching;
 import net.emaze.dysfunctional.testing.O;
 import net.emaze.dysfunctional.tuples.BinaryIdentity;
 import net.emaze.dysfunctional.tuples.Pair;
@@ -21,59 +20,59 @@ import org.junit.Test;
  *
  * @author rferranti
  */
-public class AdaptersTest {
+public class DispatchingTest {
 
     @Test
     public void canAdaptActionToDelegate() {
-        final Delegate<Void, O> adapted = Dispatching.action2delegate(new Noop<O>());
+        final Delegate<Void, O> adapted = Dispatching.delegate(new Noop<O>());
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canAdaptBinActionToBinDelegate() {
-        final BinaryDelegate<Void, O, O> adapted = Dispatching.action2delegate(new BinaryNoop<O, O>());
+        final BinaryDelegate<Void, O, O> adapted = Dispatching.delegate(new BinaryNoop<O, O>());
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canAdaptTerActionToTerDelegate() {
-        final TernaryDelegate<Void, O, O, O> adapted = Dispatching.action2delegate(new TernaryNoop<O, O, O>());
+        final TernaryDelegate<Void, O, O, O> adapted = Dispatching.delegate(new TernaryNoop<O, O, O>());
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canBind() {
-        final Provider<O> adapted = Dispatching.bind(new Identity<O>(), O.ONE);
+        final Provider<O> adapted = Dispatching.curry(new Identity<O>(), O.ONE);
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canBindFirstOfTwo() {
-        final Delegate<Pair<O, O>, O> adapted = Dispatching.bind1st(new BinaryIdentity<O, O>(), O.ONE);
+        final Delegate<Pair<O, O>, O> adapted = Dispatching.curry(new BinaryIdentity<O, O>(), O.ONE);
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canBindFirstOfThree() {
-        final BinaryDelegate<Triple<O, O, O>, O, O> adapted = Dispatching.bind1st(new TernaryIdentity<O, O, O>(), O.ONE);
+        final BinaryDelegate<Triple<O, O, O>, O, O> adapted = Dispatching.curry(new TernaryIdentity<O, O, O>(), O.ONE);
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canBindSecondOfTwo() {
-        final Delegate<Pair<O, O>, O> adapted = Dispatching.bind2nd(new BinaryIdentity<O, O>(), O.ONE);
+        final Delegate<Pair<O, O>, O> adapted = Dispatching.mcurry(new BinaryIdentity<O, O>(), O.ONE);
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canBindSecondOfThree() {
-        final BinaryDelegate<Triple<O, O, O>, O, O> adapted = Dispatching.bind2nd(new TernaryIdentity<O, O, O>(), O.ONE);
+        final BinaryDelegate<Triple<O, O, O>, O, O> adapted = Dispatching.mcurry(new TernaryIdentity<O, O, O>(), O.ONE);
         Assert.assertNotNull(adapted);
     }
 
     @Test
     public void canBindThirdOfThree() {
-        final BinaryDelegate<Triple<O, O, O>, O, O> adapted = Dispatching.bind3rd(new TernaryIdentity<O, O, O>(), O.ONE);
+        final BinaryDelegate<Triple<O, O, O>, O, O> adapted = Dispatching.rcurry(new TernaryIdentity<O, O, O>(), O.ONE);
         Assert.assertNotNull(adapted);
     }
 }
