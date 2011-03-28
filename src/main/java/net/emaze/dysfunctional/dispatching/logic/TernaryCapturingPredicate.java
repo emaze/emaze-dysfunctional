28a92264adf4313811966b9d93c9eaac1698a579
Roberto Ferranti <rf@emaze.net>
2011-03-28 12:21:24 +0200

    enh: captures

--- src/main/java/net/emaze/dysfunctional/dispatching/logic/TernaryCapturingPredicate.java
+++ src/main/java/net/emaze/dysfunctional/dispatching/logic/TernaryCapturingPredicate.java
@@ -1,7 +1,6 @@
 package net.emaze.dysfunctional.dispatching.logic;
 
 import net.emaze.dysfunctional.contracts.dbc;
-import net.emaze.dysfunctional.dispatching.logic.TernaryPredicate;
 import net.emaze.dysfunctional.options.Box;
 
 public class TernaryCapturingPredicate<T1, T2, T3> implements TernaryPredicate<T1, T2, T3> {
@@ -9,7 +8,6 @@ public class TernaryCapturingPredicate<T1, T2, T3> implements TernaryPredicate<T
     public final Box<T1> first = new Box<T1>();
     public final Box<T2> second = new Box<T2>();
     public final Box<T3> third = new Box<T3>();
-
     public final TernaryPredicate<T1, T2, T3> nested;
 
     public TernaryCapturingPredicate(TernaryPredicate<T1, T2, T3> nested) {
