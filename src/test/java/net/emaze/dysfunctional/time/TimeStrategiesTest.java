5db369632b98cd7ea4bcab19ae9e2f35cf09e79e
Roberto Ferranti <rf@emaze.net>
2011-05-06 16:01:17 +0200

    ref: tests for TimeStrategies are now in a suite

---
+++ src/test/java/net/emaze/dysfunctional/time/TimeStrategiesTest.java
@@ -0,0 +1,102 @@
+package net.emaze.dysfunctional.time;
+
+import java.util.concurrent.TimeUnit;
+import net.emaze.dysfunctional.options.Box;
+import net.emaze.dysfunctional.tuples.Pair;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author rferranti
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    TimeStrategiesTest.InterruptedSleepYieldsException.class,
+    TimeStrategiesTest.UninterruptedSleepWaitsForDuration.class,
+    TimeStrategiesTest.Resolutions.class
+})
+public class TimeStrategiesTest {
+
+    @RunWith(Theories.class)
+    public static class InterruptedSleepYieldsException {
+
+        @DataPoint
+        public static EpochTimeStrategy epochStrategy = new EpochTimeStrategy();
+        @DataPoint
+        public static HiResTimeStrategy hiResStrategy = new HiResTimeStrategy();
+
+        @Theory
+        public void interruptingSleepYieldsException(final TimeStrategy strategy) throws InterruptedException {
+            final Box<Boolean> shouldFail = new Box<Boolean>();
+            shouldFail.setContent(Boolean.FALSE);
+            final Thread thread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    strategy.sleep(30, TimeUnit.SECONDS);
+                    shouldFail.setContent(Boolean.TRUE);
+                }
+            });
+            thread.start();
+            thread.interrupt();
+            thread.join();
+            Assert.assertFalse(shouldFail.getContent());
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class UninterruptedSleepWaitsForDuration {
+
+        @DataPoint
+        public static EpochTimeStrategy epochStrategy = new EpochTimeStrategy();
+        @DataPoint
+        public static HiResTimeStrategy hiResStrategy = new HiResTimeStrategy();
+        @DataPoint
+        public static WarpingTimeStrategy warpingStrategy = new WarpingTimeStrategy(0);
+
+        @Theory
+        public void uninterruptedSleepWaitsAtLeastTheDuration(final TimeStrategy strategy) throws InterruptedException {
+            final long expectedMillis = 10;
+            Thread thread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    strategy.sleep(expectedMillis, TimeUnit.MILLISECONDS);
+                }
+            });
+            final Pair<Long, TimeUnit> start = strategy.currentTime();
+            thread.start();
+            thread.join();
+            final Pair<Long, TimeUnit> stop = strategy.currentTime();
+            final Long durationInMillis = stop.second().toMillis(stop.first() - start.first());
+            Assert.assertTrue(durationInMillis >= expectedMillis);
+        }
+    }
+
+    public static class Resolutions {
+
+        @Test
+        public void epochTimeUnitInCurrentTimeIsInMillis() {
+            final TimeUnit got = new EpochTimeStrategy().currentTime().second();
+            Assert.assertEquals(TimeUnit.MILLISECONDS, got);
+        }
+
+        @Test
+        public void hiResTimeUnitInCurrentTimeIsInNanos() {
+            final TimeUnit got = new HiResTimeStrategy().currentTime().second();
+            Assert.assertEquals(TimeUnit.NANOSECONDS, got);
+        }
+
+        @Test
+        public void warpingTimeUnitInCurrentTimeIsInMillis() {
+            final TimeUnit got = new WarpingTimeStrategy(0l).currentTime().second();
+            Assert.assertEquals(TimeUnit.MILLISECONDS, got);
+        }
+    }
+}
