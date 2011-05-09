b80a75410aab06690e3a86645da242eaf6a47167
Roberto Ferranti <rf@emaze.net>
2011-05-09 11:47:36 +0200

    enh: dbc coverage

---
+++ src/test/java/net/emaze/dysfunctional/contracts/DbcTest.java
@@ -0,0 +1,27 @@
+package net.emaze.dysfunctional.contracts;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rferranti
+ */
+public class DbcTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failingPreconditionYieldsIllegalArgumentException() {
+        dbc.precondition(false, "i'm a precondition failed message");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void failingStatePreconditionYieldsIllegalStateException() {
+        dbc.stateprecondition(false, "i'm a precondition failed message");
+    }
+
+    @Test
+    public void dbcFacadeIsNotFinal() {
+        new dbc() {
+        };
+    }
+}
