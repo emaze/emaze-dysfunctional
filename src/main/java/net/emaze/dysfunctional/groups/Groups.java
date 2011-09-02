fd1008aabd5f1e2186609f8aab6efeb14cdaa433
Davide Grohmann <davide.grohmann@emaze.net>
2011-09-02 14:00:26 +0200

    dev: added support for arrays in Groups

--- src/main/java/net/emaze/dysfunctional/groups/Groups.java
+++ src/main/java/net/emaze/dysfunctional/groups/Groups.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
+import net.emaze.dysfunctional.adapting.ArrayIterator;
 import net.emaze.dysfunctional.collections.ArrayListFactory;
 import net.emaze.dysfunctional.collections.HashMapFactory;
 import net.emaze.dysfunctional.contracts.dbc;
@@ -20,12 +21,20 @@ public abstract class Groups {
         return groupBy(groupies, grouper, new ArrayListFactory<V>(), new HashMapFactory<K, ArrayList<V>>());
     }
 
+    public static <K, V> Map<K, ArrayList<V>> groupBy(V[] groupies, Delegate<K, V> grouper) {
+        return groupBy(new ArrayIterator<V>(groupies), grouper, new ArrayListFactory<V>(), new HashMapFactory<K, ArrayList<V>>());
+    }
+
     public static <K, V> Map<K, ArrayList<V>> groupBy(Iterator<V> groupies, Delegate<K, V> grouper) {
         return groupBy(groupies, grouper, new ArrayListFactory<V>(), new HashMapFactory<K, ArrayList<V>>());
     }
 
     public static <C extends Collection<V>, K, V> Map<K, C> groupBy(Iterable<V> groupies, Delegate<K, V> grouper, Provider<C> collectionProvider) {
         return groupBy(groupies, grouper, collectionProvider, new HashMapFactory<K, C>());
+    }
+
+    public static <C extends Collection<V>, K, V> Map<K, C> groupBy(V[] groupies, Delegate<K, V> grouper, Provider<C> collectionProvider) {
+        return groupBy(new ArrayIterator<V>(groupies), grouper, collectionProvider, new HashMapFactory<K, C>());
     }
 
     public static <C extends Collection<V>, K, V> Map<K, C> groupBy(Iterator<V> groupies, Delegate<K, V> grouper, Provider<C> collectionProvider) {
@@ -35,6 +44,10 @@ public abstract class Groups {
     public static <VC extends Collection<V>, M extends Map<K, VC>, C extends VC, K, V> Map<K, VC> groupBy(Iterable<V> groupies, Delegate<K, V> grouper, Provider<C> collectionProvider, Provider<M> mapProvider) {
         dbc.precondition(groupies != null, "cannot group with a null iterable");
         return groupBy(groupies.iterator(), grouper, collectionProvider, mapProvider);
+    }
+
+    public static <VC extends Collection<V>, M extends Map<K, VC>, C extends VC, K, V> Map<K, VC> groupBy(V[] groupies, Delegate<K, V> grouper, Provider<C> collectionProvider, Provider<M> mapProvider) {
+        return groupBy(new ArrayIterator<V>(groupies), grouper, collectionProvider, mapProvider);
     }
 
     public static <VC extends Collection<V>, M extends Map<K, VC>, C extends VC, K, V> Map<K, VC> groupBy(Iterator<V> groupies, Delegate<K, V> grouper, Provider<C> collectionProvider, Provider<M> mapProvider) {
