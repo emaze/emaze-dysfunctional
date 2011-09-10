97125b25bd4874f063786467b685dac7205d45b4
Roberto Ferranti <roberto.ferranti@gmail.com>
2011-09-10 14:02:36 +0200

    ref: unused imports

--- src/main/java/net/emaze/dysfunctional/iterations/Iterations.java
+++ src/main/java/net/emaze/dysfunctional/iterations/Iterations.java
@@ -1,10 +1,6 @@
 package net.emaze.dysfunctional.iterations;
 
-import net.emaze.dysfunctional.dispatching.Tapper;
 import java.util.Iterator;
-import net.emaze.dysfunctional.contracts.dbc;
-import net.emaze.dysfunctional.dispatching.actions.Action;
-import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 
 /**
  * iterating shortcuts (usage shouldn't be abused)
