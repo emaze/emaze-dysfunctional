00cee02f1fc7c6014fb03324c4888698cab108dc
Roberto Ferranti <roberto.ferranti@gmail.com>
2011-09-17 22:54:08 +0200

    enh: coverage on Empty{Iterator,Iterable}

---
+++ src/test/java/net/emaze/dysfunctional/iterations/EmptyIteratorTest.java
@@ -0,0 +1,32 @@
+package net.emaze.dysfunctional.iterations;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import net.emaze.dysfunctional.testing.O;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author rferranti
+ */
+public class EmptyIteratorTest {
+
+    @Test
+    public void emptyIteratorHasNoElements() {
+        final Iterator<O> iterator = new EmptyIterator<O>();
+        Assert.assertFalse(iterator.hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void callingNextYieldsNoSuchElementsException() {
+        final Iterator<O> iterator = new EmptyIterator<O>();
+        iterator.next();
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void removingFromEmptyIteratorYieldsException() {
+        final Iterator<O> iterator = new EmptyIterator<O>();
+        iterator.remove();
+    }
+}
