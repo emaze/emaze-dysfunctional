a7c35dbf441c5a757cfaf4a2284e19664a460a2d
Roberto Ferranti <rf@emaze.net>
2011-10-07 19:00:29 +0200

    enh: precondition on EndoDelegateComposer

--- src/main/java/net/emaze/dysfunctional/dispatching/delegates/EndoDelegatesComposer.java
+++ src/main/java/net/emaze/dysfunctional/dispatching/delegates/EndoDelegatesComposer.java
@@ -1,12 +1,14 @@
 package net.emaze.dysfunctional.dispatching.delegates;
 
 import java.util.Iterator;
+import net.emaze.dysfunctional.contracts.dbc;
 import net.emaze.dysfunctional.dispatching.Dispatching;
 
 public class EndoDelegatesComposer<T> implements Delegate<Delegate<T, T>, Iterator<Delegate<T, T>>> {
 
     @Override
     public Delegate<T, T> perform(Iterator<Delegate<T, T>> endoDelegates) {
+        dbc.precondition(endoDelegates != null, "cannot compose a null iterator of endoDelegates");
         Delegate<T, T> current = new Identity<T>();
         while (endoDelegates.hasNext()) {
             current = Dispatching.compose(endoDelegates.next(), current);
