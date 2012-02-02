960d39c6567e009082caf54e6026e8a101884791
Roberto Ferranti <rf@emaze.net>
2012-02-02 17:56:50 +0100

    enh: javadoc

--- src/main/java/net/emaze/dysfunctional/collections/EntryToPair.java
+++ src/main/java/net/emaze/dysfunctional/collections/EntryToPair.java
@@ -5,6 +5,13 @@ import net.emaze.dysfunctional.contracts.dbc;
 import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import net.emaze.dysfunctional.tuples.Pair;
 
+/**
+ * Transforms a Map.Entry to a Pair
+ *
+ * @author rferranti, sbaruzza
+ * @param <K> the key type
+ * @param <V> the value type
+ */
 public class EntryToPair<K, V> implements Delegate<Pair<K, V>, Entry<K, V>> {
 
     @Override
--- src/main/java/net/emaze/dysfunctional/collections/MapToPairs.java
+++ src/main/java/net/emaze/dysfunctional/collections/MapToPairs.java
@@ -8,6 +8,13 @@ import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 import net.emaze.dysfunctional.iterations.TransformingIterator;
 import net.emaze.dysfunctional.tuples.Pair;
 
+/**
+ * Transforms a Map to an Iterator of pairs.
+ *
+ * @author rferranti, sbaruzza
+ * @param <K> the key type
+ * @param <V> the value type
+ */
 public class MapToPairs<K, V> implements Delegate<Iterator<Pair<K, V>>, Map<K, V>> {
 
     @Override
@@ -15,5 +22,4 @@ public class MapToPairs<K, V> implements Delegate<Iterator<Pair<K, V>>, Map<K, V
         dbc.precondition(map != null, "cannot transform a null map to an iterator of pairs");
         return new TransformingIterator<Pair<K, V>, Entry<K, V>>(map.entrySet().iterator(), new EntryToPair<K, V>());
     }
-
 }
