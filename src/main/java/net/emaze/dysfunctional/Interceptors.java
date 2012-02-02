b6ed10abdbeb42166cff0b698d0dabf542278ad1
Roberto Ferranti <rf@emaze.net>
2012-02-02 11:31:44 +0100

    ref: Interceptors.chain is now Interceptors.intercept

--- src/main/java/net/emaze/dysfunctional/Interceptors.java
+++ src/main/java/net/emaze/dysfunctional/Interceptors.java
@@ -16,19 +16,19 @@ import net.emaze.dysfunctional.iterations.SingletonIterator;
 
 public abstract class Interceptors {
 
-    public <R, T> Delegate<R, T> chain(Delegate<R, T> innermost, Interceptor<T> interceptor) {
+    public <R, T> Delegate<R, T> intercept(Delegate<R, T> innermost, Interceptor<T> interceptor) {
         dbc.precondition(interceptor != null, "cannot create an interceptor chain with a null interceptor");
         return new InterceptorChain<R, T>(innermost, new SingletonIterator<Interceptor<T>>(interceptor));
     }
 
-    public <R, T> Delegate<R, T> chain(Delegate<R, T> innermost, Interceptor<T> first, Interceptor<T> second) {
+    public <R, T> Delegate<R, T> intercept(Delegate<R, T> innermost, Interceptor<T> first, Interceptor<T> second) {
         dbc.precondition(first != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(second != null, "cannot create an interceptor chain with a null interceptor");
         final ArrayIterator<Interceptor<T>> interceptors = ArrayIterator.of(first, second);
         return new InterceptorChain<R, T>(innermost, interceptors);
     }
 
-    public <R, T> Delegate<R, T> chain(Delegate<R, T> innermost, Interceptor<T> first, Interceptor<T> second, Interceptor<T> third) {
+    public <R, T> Delegate<R, T> intercept(Delegate<R, T> innermost, Interceptor<T> first, Interceptor<T> second, Interceptor<T> third) {
         dbc.precondition(first != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(second != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(third != null, "cannot create an interceptor chain with a null interceptor");
@@ -36,32 +36,32 @@ public abstract class Interceptors {
         return new InterceptorChain<R, T>(innermost, interceptors);
     }
 
-    public <R, T, I extends Interceptor<T>> Delegate<R, T> chain(Delegate<R, T> innermost, Iterable<I> interceptors) {
+    public <R, T, I extends Interceptor<T>> Delegate<R, T> intercept(Delegate<R, T> innermost, Iterable<I> interceptors) {
         dbc.precondition(interceptors != null, "cannot create an interceptor chain with a null iterable of interceptors");
         return new InterceptorChain<R, T>(innermost, interceptors.iterator());
     }
 
-    public <R, T, I extends Interceptor<T>> Delegate<R, T> chain(Delegate<R, T> innermost, Iterator<I> interceptors) {
+    public <R, T, I extends Interceptor<T>> Delegate<R, T> intercept(Delegate<R, T> innermost, Iterator<I> interceptors) {
         return new InterceptorChain<R, T>(innermost, interceptors);
     }
 
-    public <R, T> Delegate<R, T> chain(Delegate<R, T> innermost, Interceptor<T>... interceptors) {
+    public <R, T> Delegate<R, T> intercept(Delegate<R, T> innermost, Interceptor<T>... interceptors) {
         return new InterceptorChain<R, T>(innermost, new ArrayIterator<Interceptor<T>>(interceptors));
     }
 
-    public <R, T1, T2> BinaryDelegate<R, T1, T2> chain(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2> interceptor) {
+    public <R, T1, T2> BinaryDelegate<R, T1, T2> intercept(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2> interceptor) {
         dbc.precondition(interceptor != null, "cannot create an interceptor chain with a null interceptor");
         return new BinaryInterceptorChain<R, T1, T2>(innermost, new SingletonIterator<BinaryInterceptor<T1, T2>>(interceptor));
     }
 
-    public <R, T1, T2> BinaryDelegate<R, T1, T2> chain(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2> first, BinaryInterceptor<T1, T2> second) {
+    public <R, T1, T2> BinaryDelegate<R, T1, T2> intercept(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2> first, BinaryInterceptor<T1, T2> second) {
         dbc.precondition(first != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(second != null, "cannot create an interceptor chain with a null interceptor");
         final ArrayIterator<BinaryInterceptor<T1, T2>> interceptors = ArrayIterator.of(first, second);
         return new BinaryInterceptorChain<R, T1, T2>(innermost, interceptors);
     }
 
-    public <R, T1, T2> BinaryDelegate<R, T1, T2> chain(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2> first, BinaryInterceptor<T1, T2> second, BinaryInterceptor<T1, T2> third) {
+    public <R, T1, T2> BinaryDelegate<R, T1, T2> intercept(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2> first, BinaryInterceptor<T1, T2> second, BinaryInterceptor<T1, T2> third) {
         dbc.precondition(first != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(second != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(third != null, "cannot create an interceptor chain with a null interceptor");
@@ -69,32 +69,32 @@ public abstract class Interceptors {
         return new BinaryInterceptorChain<R, T1, T2>(innermost, interceptors);
     }
 
-    public <R, T1, T2, I extends BinaryInterceptor<T1, T2>> BinaryDelegate<R, T1, T2> chain(BinaryDelegate<R, T1, T2> innermost, Iterable<I> interceptors) {
+    public <R, T1, T2, I extends BinaryInterceptor<T1, T2>> BinaryDelegate<R, T1, T2> intercept(BinaryDelegate<R, T1, T2> innermost, Iterable<I> interceptors) {
         dbc.precondition(interceptors != null, "cannot create an interceptor chain with a null iterable of interceptors");
         return new BinaryInterceptorChain<R, T1, T2>(innermost, interceptors.iterator());
     }
 
-    public <R, T1, T2, I extends BinaryInterceptor<T1, T2>> BinaryDelegate<R, T1, T2> chain(BinaryDelegate<R, T1, T2> innermost, Iterator<I> interceptors) {
+    public <R, T1, T2, I extends BinaryInterceptor<T1, T2>> BinaryDelegate<R, T1, T2> intercept(BinaryDelegate<R, T1, T2> innermost, Iterator<I> interceptors) {
         return new BinaryInterceptorChain<R, T1, T2>(innermost, interceptors);
     }
 
-    public <R, T1, T2> BinaryDelegate<R, T1, T2> chain(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2>... interceptors) {
+    public <R, T1, T2> BinaryDelegate<R, T1, T2> intercept(BinaryDelegate<R, T1, T2> innermost, BinaryInterceptor<T1, T2>... interceptors) {
         return new BinaryInterceptorChain<R, T1, T2>(innermost, new ArrayIterator<BinaryInterceptor<T1, T2>>(interceptors));
     }
 
-    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> chain(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3> interceptor) {
+    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> intercept(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3> interceptor) {
         dbc.precondition(interceptor != null, "cannot create an interceptor chain with a null interceptor");
         return new TernaryInterceptorChain<R, T1, T2, T3>(innermost, new SingletonIterator<TernaryInterceptor<T1, T2, T3>>(interceptor));
     }
 
-    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> chain(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3> first, TernaryInterceptor<T1, T2, T3> second) {
+    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> intercept(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3> first, TernaryInterceptor<T1, T2, T3> second) {
         dbc.precondition(first != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(second != null, "cannot create an interceptor chain with a null interceptor");
         final ArrayIterator<TernaryInterceptor<T1, T2, T3>> interceptors = ArrayIterator.of(first, second);
         return new TernaryInterceptorChain<R, T1, T2, T3>(innermost, interceptors);
     }
 
-    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> chain(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3> first, TernaryInterceptor<T1, T2, T3> second, TernaryInterceptor<T1, T2, T3> third) {
+    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> intercept(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3> first, TernaryInterceptor<T1, T2, T3> second, TernaryInterceptor<T1, T2, T3> third) {
         dbc.precondition(first != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(second != null, "cannot create an interceptor chain with a null interceptor");
         dbc.precondition(third != null, "cannot create an interceptor chain with a null interceptor");
@@ -102,16 +102,16 @@ public abstract class Interceptors {
         return new TernaryInterceptorChain<R, T1, T2, T3>(innermost, interceptors);
     }
 
-    public <R, T1, T2, T3, I extends TernaryInterceptor<T1, T2, T3>> TernaryDelegate<R, T1, T2, T3> chain(TernaryDelegate<R, T1, T2, T3> innermost, Iterable<I> interceptors) {
+    public <R, T1, T2, T3, I extends TernaryInterceptor<T1, T2, T3>> TernaryDelegate<R, T1, T2, T3> intercept(TernaryDelegate<R, T1, T2, T3> innermost, Iterable<I> interceptors) {
         dbc.precondition(interceptors != null, "cannot create an interceptor chain with a null iterable of interceptors");
         return new TernaryInterceptorChain<R, T1, T2, T3>(innermost, interceptors.iterator());
     }
 
-    public <R, T1, T2, T3, I extends TernaryInterceptor<T1, T2, T3>> TernaryDelegate<R, T1, T2, T3> chain(TernaryDelegate<R, T1, T2, T3> innermost, Iterator<I> interceptors) {
+    public <R, T1, T2, T3, I extends TernaryInterceptor<T1, T2, T3>> TernaryDelegate<R, T1, T2, T3> intercept(TernaryDelegate<R, T1, T2, T3> innermost, Iterator<I> interceptors) {
         return new TernaryInterceptorChain<R, T1, T2, T3>(innermost, interceptors);
     }
 
-    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> chain(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3>... interceptors) {
+    public <R, T1, T2, T3> TernaryDelegate<R, T1, T2, T3> intercept(TernaryDelegate<R, T1, T2, T3> innermost, TernaryInterceptor<T1, T2, T3>... interceptors) {
         return new TernaryInterceptorChain<R, T1, T2, T3>(innermost, new ArrayIterator<TernaryInterceptor<T1, T2, T3>>(interceptors));
     }
 }
