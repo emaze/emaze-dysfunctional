9b1fc312cf9d8b3b1affc51fd4a1d32d4acfe99a
Roberto Ferranti <rf@emaze.net>
2012-02-03 14:12:14 +0100

    ref: naming

--- src/main/java/net/emaze/dysfunctional/strings/lexcasts/BooleanParser.java
+++ src/main/java/net/emaze/dysfunctional/strings/lexcasts/BooleanParser.java
@@ -7,12 +7,12 @@ import net.emaze.dysfunctional.dispatching.delegates.Delegate;
 
 public class BooleanParser implements Delegate<Boolean, String> {
 
-    private static final List<String> acceptedValues = Arrays.asList("true", "false");
+    private static final List<String> ACCEPTED_VALUES = Arrays.asList("true", "false");
 
     @Override
     public Boolean perform(String parsee) {
         dbc.precondition(parsee != null, "cannot parse a null string");
-        dbc.precondition(acceptedValues.contains(parsee.toLowerCase()), "cannot parse string '%s' to boolean", parsee);
+        dbc.precondition(ACCEPTED_VALUES.contains(parsee.toLowerCase()), "cannot parse string '%s' to boolean", parsee);
         return Boolean.parseBoolean(parsee);
     }
 }
