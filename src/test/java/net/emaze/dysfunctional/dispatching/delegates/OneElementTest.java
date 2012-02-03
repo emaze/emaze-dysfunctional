5f3bc1f6170a51acbe120f1f1b67025b1e0934e8
Roberto Ferranti <rf@emaze.net>
2012-02-03 10:29:09 +0100

    enh: coverage on OneElement

---
+++ src/test/java/net/emaze/dysfunctional/dispatching/delegates/OneElementTest.java
@@ -0,0 +1,35 @@
+package net.emaze.dysfunctional.dispatching.delegates;
+
+import java.util.Iterator;
+import junit.framework.Assert;
+import net.emaze.dysfunctional.Iterations;
+import net.emaze.dysfunctional.testing.O;
+import org.junit.Test;
+
+public class OneElementTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void consuminNullIteratorYieldsException() {
+        final Iterator<O> iterator = null;
+        new OneElement<O>().perform(iterator);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void consuminEmptyIteratorYieldsException() {
+        final Iterator<O> iterator = Iterations.iterator();
+        new OneElement<O>().perform(iterator);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void consuminIteratorWithMoreThanOneElementYieldsException() {
+        final Iterator<O> iterator = Iterations.iterator(O.ONE, O.ANOTHER);
+        new OneElement<O>().perform(iterator);
+    }
+
+    @Test
+    public void consuminIteratorWithOneElementYieldsTheElement() {
+        final Iterator<String> iterator = Iterations.iterator("the only element");
+        final String got = new OneElement<String>().perform(iterator);
+        Assert.assertEquals("the only element", got);
+    }
+}
