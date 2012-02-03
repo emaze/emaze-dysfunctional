fecb651d4b598d67c84c0df19f585f91d14e463b
Roberto Ferranti <rf@emaze.net>
2012-02-03 10:36:29 +0100

    enh: coverage

--- src/test/java/net/emaze/dysfunctional/numbers/AverageTest.java
+++ src/test/java/net/emaze/dysfunctional/numbers/AverageTest.java
@@ -4,11 +4,24 @@ import java.util.Iterator;
 import net.emaze.dysfunctional.Iterations;
 import net.emaze.dysfunctional.Reductions;
 import net.emaze.dysfunctional.numbers.operations.IntegerOperations;
+import net.emaze.dysfunctional.testing.O;
 import net.emaze.dysfunctional.tuples.Pair;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class AverageTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingWithNullPolicyYieldsException() {
+        new Average<O>(null);
+    }
+
+    @Test
+    public void canSumUsingAPolicy() {
+        final Average<Integer> average = new Average<Integer>(new IntegerOperations());
+        final Pair<Integer, Long> sumAndCount = average.perform(Pair.of(0, 0l), 6);
+        Assert.assertEquals(Pair.of(6, 1L), sumAndCount);
+    }
 
     @Test//@explorative
     public void canCalculateAverageWithReductions() {
--- src/test/java/net/emaze/dysfunctional/numbers/SumTest.java
+++ src/test/java/net/emaze/dysfunctional/numbers/SumTest.java
@@ -3,16 +3,36 @@ package net.emaze.dysfunctional.numbers;
 import java.util.Iterator;
 import net.emaze.dysfunctional.Iterations;
 import net.emaze.dysfunctional.Reductions;
+import net.emaze.dysfunctional.dispatching.delegates.BinaryDelegate;
 import net.emaze.dysfunctional.numbers.operations.IntegerOperations;
+import net.emaze.dysfunctional.numbers.policies.SumPolicy;
+import net.emaze.dysfunctional.testing.O;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class SumTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void creatingWithNullPolicyYieldsException() {
+        new Sum<O, O, O>(null);
+    }
 
-    @Test//@explorative
+    @Test
+    public void canSumUsingAPolicy() {
+        BinaryDelegate<O, O, O> sum = new Sum<O, O, O>(new SumPolicy<O, O, O>() {
+
+            @Override
+            public O sum(O lhs, O rhs) {
+                return O.YET_ANOTHER;
+            }
+        });
+        final O got = sum.perform(O.ONE, O.ANOTHER);
+        Assert.assertEquals(O.YET_ANOTHER, got);
+    }
+
+    @Test//@Sample
     public void canReduceIntegersUsingSum() {
-        Iterator<Integer> iterator = Iterations.iterator(1,2, 3);
+        Iterator<Integer> iterator = Iterations.iterator(1, 2, 3);
         Integer reduced = Reductions.reduce(iterator, new Sum<Integer, Integer, Integer>(new IntegerOperations()), 0);
         Assert.assertEquals(6, reduced.intValue());
     }
