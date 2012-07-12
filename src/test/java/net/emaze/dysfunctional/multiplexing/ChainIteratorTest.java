c68fdbf697921519ed12f0e396a9a1efaee34b3d
Fulvio Meden <fulvio.meden@emaze.net>
2012-07-12 09:58:16 +0200

    enh: ChainIterator tests

--- src/test/java/net/emaze/dysfunctional/multiplexing/ChainIteratorTest.java
+++ src/test/java/net/emaze/dysfunctional/multiplexing/ChainIteratorTest.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import net.emaze.dysfunctional.Iterations;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,30 +17,30 @@ public class ChainIteratorTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void creatingWithNullListYieldsException() {
-        Iterator<Iterator<Object>> iterators = null;
+        final Iterator<Iterator<Object>> iterators = null;
         new ChainIterator<Object>(iterators);
     }
 
     @Test
     public void emptyIteratorListHasNoNext() {
-        List<Iterator<Object>> iterators = Arrays.asList();
-        ChainIterator<Object> chain = new ChainIterator<Object>(iterators.iterator());
+        final List<Iterator<Object>> iterators = Arrays.asList();
+        final ChainIterator<Object> chain = new ChainIterator<Object>(iterators.iterator());
         Assert.assertFalse(chain.hasNext());
     }
 
     @Test
     public void emptyIteratorHasNoNext() {
-        List<Object> emptyIter = new ArrayList<Object>();
-        List<Iterator<Object>> iterators = Arrays.asList(emptyIter.iterator());
-        ChainIterator<Object> chain = new ChainIterator<Object>(iterators.iterator());
+        final List<Object> emptyIter = new ArrayList<Object>();
+        final Iterator<Iterator<Object>> iterators = Iterations.iterator(emptyIter.iterator());
+        final ChainIterator<Object> chain = new ChainIterator<Object>(iterators);
         Assert.assertFalse(chain.hasNext());
     }
 
     @Test
     public void nonEmptyIteratorHasNext() {
-        List<Object> nonEmptyIter = Arrays.<Object>asList(1);
-        List<Iterator<Object>> iterators = Arrays.asList(nonEmptyIter.iterator());
-        ChainIterator<Object> chain = new ChainIterator<Object>(iterators.iterator());
+        final List<Object> nonEmptyIter = Arrays.<Object>asList(1);
+        final Iterator<Iterator<Object>> iterators = Iterations.iterator(nonEmptyIter.iterator());
+        final ChainIterator<Object> chain = new ChainIterator<Object>(iterators);
         Assert.assertTrue(chain.hasNext());
     }
 
@@ -47,8 +48,8 @@ public class ChainIteratorTest {
     public void iteratorsAreChained(){
         final List<Integer> input1 = Arrays.asList(1,2);
         final List<Integer> input2 = Arrays.asList(3,4);
-        final Iterator<Iterator<Integer>> iteratorsIterator = Arrays.asList(input1.iterator(), input2.iterator()).iterator();
-        Iterator<Integer> iter = new ChainIterator(iteratorsIterator);
+        final Iterator<Iterator<Integer>> iteratorsIterator = Iterations.iterator(input1.iterator(), input2.iterator());
+        final Iterator<Integer> iter = new ChainIterator<Integer>(iteratorsIterator);
         final List<Integer> got = new ArrayList<Integer>();
         while(iter.hasNext()){
             got.add(iter.next());
@@ -59,8 +60,8 @@ public class ChainIteratorTest {
     @Test
     public void sameCollectionCanBeChainedTwoTimes(){
         final List<Integer> input = Arrays.asList(1,2);
-        final Iterator<Iterator<Integer>> iteratorsIterator = Arrays.asList(input.iterator(), input.iterator()).iterator();
-        Iterator<Integer> iter = new ChainIterator(iteratorsIterator);
+        final Iterator<Iterator<Integer>> iteratorsIterator = Iterations.iterator(input.iterator(), input.iterator());
+        final Iterator<Integer> iter = new ChainIterator<Integer>(iteratorsIterator);
         final List<Integer> got = new ArrayList<Integer>();
         while(iter.hasNext()){
             got.add(iter.next());
@@ -71,24 +72,33 @@ public class ChainIteratorTest {
     @Test
     public void twoEmptyIteratorsLeadToAnEmptyIterator(){
         final List<Integer> input = new ArrayList<Integer>();
-        final Iterator<Iterator<Integer>> iteratorsIterator = Arrays.asList(input.iterator(), input.iterator()).iterator();
-        Iterator<Integer> iter = new ChainIterator(iteratorsIterator);
+        final Iterator<Iterator<Integer>> iteratorsIterator = Iterations.iterator(input.iterator(), input.iterator());
+        final Iterator<Integer> iter = new ChainIterator<Integer>(iteratorsIterator);
         Assert.assertEquals(false,iter.hasNext());
     }
     
     @Test(expected=NoSuchElementException.class)
     public void consumingtwoEmptyIteratorsThrowsAnException(){
         final List<Integer> input = new ArrayList<Integer>();
-        final Iterator<Iterator<Integer>> iteratorsIterator = Arrays.asList(input.iterator(), input.iterator()).iterator();
-        Iterator<Integer> iter = new ChainIterator(iteratorsIterator);
+        final Iterator<Iterator<Integer>> iteratorsIterator = Iterations.iterator(input.iterator(), input.iterator());
+        final Iterator<Integer> iter = new ChainIterator<Integer>(iteratorsIterator);
         iter.next();
     }
 
     @Test
     public void iteratorsIteratorIsNotConsumedUntilUsed() {
         final List<Integer> input = Arrays.asList(1,2);
-        final Iterator<Iterator<Integer>> iteratorsIterator = Arrays.asList(input.iterator()).iterator();
-        Iterator<Integer> iter = new ChainIterator(iteratorsIterator);
+        final Iterator<Iterator<Integer>> iteratorsIterator = Iterations.iterable(input.iterator()).iterator();
+        final Iterator<Integer> iter = new ChainIterator<Integer>(iteratorsIterator);
+        Assert.assertTrue(iteratorsIterator.hasNext());
+    }
+    
+    @Test
+    public void iteratorsIteratorIsNotConsumedEagerlyOnNext() {
+        final List<Integer> input = Arrays.asList(1,2);
+        final Iterator<Iterator<Integer>> iteratorsIterator = Iterations.iterator(input.iterator(), input.iterator());
+        final Iterator<Integer> iter = new ChainIterator<Integer>(iteratorsIterator);
+        iter.next();
         Assert.assertTrue(iteratorsIterator.hasNext());
     }
 }
