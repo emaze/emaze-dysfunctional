3c00254b18ab6807de3cd9016948e5f517bb85aa
Roberto Ferranti <rf@emaze.net>
2012-11-14 17:13:36 +0100

    ref: RoundRobinLongestIterator is now lazy; fix: regressions on RoundRobinLongestIterator (stopped iterating when the first iterator ended)

--- src/main/java/net/emaze/dysfunctional/multiplexing/RoundRobinLongestIterator.java
+++ src/main/java/net/emaze/dysfunctional/multiplexing/RoundRobinLongestIterator.java
@@ -1,18 +1,13 @@
 package net.emaze.dysfunctional.multiplexing;
 
-import java.util.ArrayList;
+import java.util.Deque;
 import java.util.Iterator;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import net.emaze.dysfunctional.contracts.dbc;
-import net.emaze.dysfunctional.dispatching.logic.HasNext;
 import net.emaze.dysfunctional.iterations.ReadOnlyIterator;
-import net.emaze.dysfunctional.iterations.TransformingIterator;
+import net.emaze.dysfunctional.options.Box;
 import net.emaze.dysfunctional.options.Maybe;
-import net.emaze.dysfunctional.options.MaybeIteratorTransformer;
-import net.emaze.dysfunctional.order.NextIntegerSequencingPolicy;
-import net.emaze.dysfunctional.order.PeriodicIterator;
-import net.emaze.dysfunctional.order.PeriodicSequencingPolicy;
-import net.emaze.dysfunctional.reductions.Any;
 
 /**
  * squared: [1,2] [a,b,c] -> just(1),just(a),just(2),just(b),nothing,just(c)
@@ -22,26 +17,53 @@ import net.emaze.dysfunctional.reductions.Any;
  */
 public class RoundRobinLongestIterator<E> extends ReadOnlyIterator<Maybe<E>> {
 
-    private final List<Iterator<Maybe<E>>> iterators = new ArrayList<Iterator<Maybe<E>>>();
-    private final PeriodicIterator<Integer> indexSelector;
+    private final Iterator<? extends Iterator<E>> iterators;
+    private final Deque<Iterator<E>> memory = new LinkedList<Iterator<E>>();
+    private final Box<Iterator<E>> prefetched = Box.empty();
+    private int fetchedCounter;
 
     public <T extends Iterator<E>> RoundRobinLongestIterator(Iterator<T> iterators) {
-        dbc.precondition(iterators != null, "trying to create a PreciseMultiplexingIterator from a null iterator of iterators");
-        final Iterator<Iterator<Maybe<E>>> transformed = new TransformingIterator<Iterator<Maybe<E>>, T>(iterators, new MaybeIteratorTransformer<T, E>());
-        while (transformed.hasNext()) {
-            this.iterators.add(transformed.next());
-        }
-        final PeriodicSequencingPolicy<Integer> period = new PeriodicSequencingPolicy<Integer>(new NextIntegerSequencingPolicy(), 0, this.iterators.size() - 1);
-        this.indexSelector = new PeriodicIterator<Integer>(period, 0);
+        dbc.precondition(iterators != null, "trying to create a RoundRobinLongestIterator from a null iterator of iterators");
+        this.iterators = iterators;
     }
 
     @Override
     public boolean hasNext() {
-        return new Any<Iterator<Maybe<E>>>(new HasNext<Iterator<Maybe<E>>>()).accept(iterators.iterator());
+        prefetchAndMemorize();
+        return !empty();
+    }
+
+    private boolean empty() {
+        return !prefetched.hasContent() || (!prefetched.getContent().hasNext() && fetchedCounter % memory.size() == 0);
     }
 
     @Override
     public Maybe<E> next() {
-        return iterators.get(indexSelector.next()).next();
+        prefetchAndMemorize();
+        if (empty()) {
+            throw new NoSuchElementException();
+        }
+        ++fetchedCounter;
+        final Iterator<E> current = prefetched.unload().value();
+        return current.hasNext() ? Maybe.just(current.next()) : Maybe.<E>nothing();
+    }
+
+    private void prefetchAndMemorize() {
+        if (prefetched.hasContent()) {
+            return;
+        }
+        if (iterators.hasNext()) {
+            final Iterator<E> iterator = iterators.next();
+            prefetched.setContent(iterator);
+            memory.push(iterator);
+            return;
+        }
+        if (memory.isEmpty()) {
+            return;
+        }
+        fetchedCounter = fetchedCounter % memory.size();
+        final Iterator<E> iter = memory.removeLast();
+        prefetched.setContent(iter);
+        memory.push(iter);
     }
 }
--- src/test/java/net/emaze/dysfunctional/multiplexing/RoundRobinLongestIteratorTest.java
+++ src/test/java/net/emaze/dysfunctional/multiplexing/RoundRobinLongestIteratorTest.java
@@ -3,8 +3,11 @@ package net.emaze.dysfunctional.multiplexing;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import net.emaze.dysfunctional.Consumers;
+import net.emaze.dysfunctional.Iterations;
 import net.emaze.dysfunctional.options.Maybe;
+import net.emaze.dysfunctional.testing.O;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -49,6 +52,46 @@ public class RoundRobinLongestIteratorTest {
             List<Maybe<Integer>> got = Consumers.all(iter);
             List<Maybe<Integer>> expected = Arrays.asList(Maybe.just(1), Maybe.just(2), Maybe.just(3), Maybe.just(4));
             Assert.assertEquals(expected, got);
+        }
+
+        @Test
+        public void gretestIteratorSizeIsTheChannelSize() {
+            Iterator<Integer> odds = Arrays.asList(1, 3, 5).iterator();
+            Iterator<Integer> evens = Arrays.asList(2, 4).iterator();
+            Iterator<Iterator<Integer>> oddsAndEvens = Arrays.asList(odds, evens).iterator();
+            RoundRobinLongestIterator<Integer> iter = new RoundRobinLongestIterator<Integer>(oddsAndEvens);
+            List<Maybe<Integer>> got = Consumers.all(iter);
+            List<Maybe<Integer>> expected = Arrays.asList(Maybe.just(1), Maybe.just(2), Maybe.just(3), Maybe.just(4), Maybe.just(5), Maybe.<Integer>nothing());
+            Assert.assertEquals(expected, got);
+        }
+
+        @Test
+        public void canEvaluatesizeWithMultipleChannels() {
+            final Iterator<Integer> odds = Arrays.asList(1, 3, 5).iterator();
+            final Iterator<Integer> evens = Arrays.<Integer>asList().iterator();
+            final Iterator<Iterator<Integer>> oddsAndEvens = Arrays.asList(odds, evens, evens, evens).iterator();
+            final RoundRobinLongestIterator<Integer> iter = new RoundRobinLongestIterator<Integer>(oddsAndEvens);
+            final int size = Consumers.all(iter).size();
+            Assert.assertEquals(12, size);
+        }
+
+        @Test
+        public void creatingWithEmptyIteratorOfIteratorsYieldsEmptyIterator() {
+            RoundRobinLongestIterator<O> iterator = new RoundRobinLongestIterator<O>(Iterations.<Iterator<O>>iterator());
+            Assert.assertFalse(iterator.hasNext());
+        }
+
+        @Test(expected = NoSuchElementException.class)
+        public void callingNextOnEmptyIteratorYieldsException() {
+            RoundRobinLongestIterator<O> iterator = new RoundRobinLongestIterator<O>(Iterations.<Iterator<O>>iterator());
+            iterator.next();
+        }
+
+        @Test(expected = NoSuchElementException.class)
+        public void callingNextAfterBoundsYieldsException() {
+            RoundRobinLongestIterator<O> iterator = new RoundRobinLongestIterator<O>(Iterations.iterator(Iterations.iterator(O.ONE)));
+            iterator.next();
+            iterator.next();
         }
     }
 }
