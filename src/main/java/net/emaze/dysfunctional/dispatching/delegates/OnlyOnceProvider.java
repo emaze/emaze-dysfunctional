b6538ad781a8994eb4cf67d8f6770a004b596d4b
Mauro Rocchi <maurocchi@emaze.net>
2014-02-25 11:50:00 +0100

    enh: OnlyOnceProvider documentation

--- src/main/java/net/emaze/dysfunctional/dispatching/delegates/OnlyOnceProvider.java
+++ src/main/java/net/emaze/dysfunctional/dispatching/delegates/OnlyOnceProvider.java
@@ -3,7 +3,8 @@ package net.emaze.dysfunctional.dispatching.delegates;
 import net.emaze.dysfunctional.contracts.dbc;
 
 /**
- * A lazy proxy that evaluate the value for the given provider only once.
+ * A proxy that evaluate the value for the given provider only once.
+ * Useful to implement lazy evaluation.
  *
  * @param <T> the provider parameter type
  */
@@ -13,7 +14,7 @@ public class OnlyOnceProvider<T> implements Provider<T> {
     private T value = null;
 
     public OnlyOnceProvider(Provider<T> provider) {
-        dbc.precondition(provider != null, "Cannot create a proxy provider with a null provider");
+        dbc.precondition(provider != null, "Cannot create an only once provider with a null provider");
         this.provider = provider;
     }
 
