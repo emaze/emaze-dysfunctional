7ed9a8d8b8b0760b03c1356ff06c76236c459df0
Mauro Rocchi <maurocchi@emaze.net>
2015-02-19 18:08:04 +0100

    ref: naming

--- src/main/java/net/emaze/dysfunctional/options/OptionalToEither.java
+++ src/main/java/net/emaze/dysfunctional/options/OptionalToEither.java
@@ -13,18 +13,18 @@ import net.emaze.dysfunctional.contracts.dbc;
  * @param <L> the either left type parameter
  * @param <R> the either right type parameter
  */
-public class MaybeToEither<L, R> implements Function<Optional<R>, Either<L, R>> {
+public class OptionalToEither<L, R> implements Function<Optional<R>, Either<L, R>> {
 
     private final Supplier<L> left;
 
-    public MaybeToEither(Supplier<L> left) {
-        dbc.precondition(left != null, "cannot create MaybeToEither with a null left supplier");
+    public OptionalToEither(Supplier<L> left) {
+        dbc.precondition(left != null, "cannot create OptionalToEither with a null left supplier");
         this.left = left;
     }
 
     @Override
     public Either<L, R> apply(Optional<R> right) {
-        dbc.precondition(right != null, "cannot transform a null maybe to an either");
+        dbc.precondition(right != null, "cannot transform a null optional to an either");
         if (right.isPresent()) {
             return Either.right(right.get());
         }
--- src/test/java/net/emaze/dysfunctional/options/OptionalToEitherTest.java
+++ src/test/java/net/emaze/dysfunctional/options/OptionalToEitherTest.java
@@ -7,28 +7,28 @@ import net.emaze.dysfunctional.testing.O;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MaybeToEitherTest {
+public class OptionalToEitherTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void creatingWithNullProviderYieldsException() {
-        new MaybeToEither<O, O>(null);
+        new OptionalToEither<O, O>(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void callingOnNullMaybeYieldsException() {
-        new MaybeToEither<O, O>(new ConstantProvider<O>(O.ANOTHER)).apply(null);
+        new OptionalToEither<O, O>(new ConstantProvider<O>(O.ANOTHER)).apply(null);
     }
 
     @Test
     public void transformingNothingYieldsLeft() {
-        final Function<Optional<O>, Either<O, O>> maybeToEither = new MaybeToEither<O, O>(new ConstantProvider<O>(O.ANOTHER));
+        final Function<Optional<O>, Either<O, O>> maybeToEither = new OptionalToEither<O, O>(new ConstantProvider<O>(O.ANOTHER));
         final Either<O, O> got = maybeToEither.apply(Optional.<O>empty());
         Assert.assertEquals(Either.left(O.ANOTHER), got);
     }
 
     @Test
     public void transformingJustYieldsRight() {
-        final Function<Optional<O>, Either<O, O>> maybeToEither = new MaybeToEither<O, O>(new ConstantProvider<O>(O.ANOTHER));
+        final Function<Optional<O>, Either<O, O>> maybeToEither = new OptionalToEither<O, O>(new ConstantProvider<O>(O.ANOTHER));
         final Either<O, O> got = maybeToEither.apply(Optional.<O>of(O.ONE));
         Assert.assertEquals(Either.right(O.ONE), got);
     }
